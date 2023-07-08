1ac11f07cbd295accc62eab8a5edbd5470fae40c
Callum Gill <callum.j.gill@googlemail.com>
2023-07-08 04:33:17 +0100

    Add CopyToClipboardButton

---
+++ frontend/src/components/buttons/CopyToClipboardButton.tsx
@@ -0,0 +1,27 @@
+import { Button } from "react-bootstrap";
+import ButtonSpinner from "./ButtonSpinner";
+import { useState } from "react";
+
+interface CopyToClipboardButtonProps {
+  text: string;
+}
+
+const CopyToClipboardButton = (props: CopyToClipboardButtonProps) => {
+  const { text } = { ...props };
+
+  const [copying, setCopying] = useState<boolean>(false);
+
+  const handleClick = async () => {
+    setCopying(true);
+    await navigator.clipboard.writeText(text);
+    setCopying(false);
+  };
+
+  return (
+    <Button variant="secondary" onClick={handleClick}>
+      {copying ? <ButtonSpinner /> : "Copy"}
+    </Button>
+  );
+};
+
+export default CopyToClipboardButton;
