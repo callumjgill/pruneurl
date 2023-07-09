102f18ad127abc1c699aee82b8aee55b37f58207
Callum Gill <callum.j.gill@googlemail.com>
2023-07-09 23:22:03 +0100

    feat(code): add useBreakpoint hook
    New custom hook for getting a breakpoint value based on the screen
    width.

---
+++ frontend/src/utils/breakpoints.ts
@@ -0,0 +1,12 @@
+export const smallScreen = 576;
+export const mediumScreen = 768;
+export const largeScreen = 992;
+export const extraLargeScreen = 1200;
+
+export const breakpointExtraSmallScreen = "xs";
+export const breakpointSmallScreen = "sm";
+export const breakpointMediumScreen = "md";
+export const breakpointLargeScreen = "lg";
+export const breakpointExtraLargeScreen = "xl";
+
+export type breakpoint = "xs" | "sm" | "md" | "lg" | "xl";
