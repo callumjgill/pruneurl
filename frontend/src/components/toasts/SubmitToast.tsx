91bca2f9d81a22a4c924e2f475480ef44c3ffc6a
Callum Gill <callum.j.gill@googlemail.com>
2023-07-09 12:44:01 +0100

    feat(Toasts): add toast transitions
    Fade wraps the toast element and only calls the callback to the parent
    once it has fully exited the transition. This ensures the toast isn't
    unmounted prematurely.

--- frontend/src/components/toasts/SubmitToast.tsx
+++ frontend/src/components/toasts/SubmitToast.tsx
@@ -1,35 +1,51 @@
-import { Toast } from "react-bootstrap";
-import SmallTimeAgo from "../TimeAgo/SmallTimeAgo";
 import { useState } from "react";
+import { Fade, Toast } from "react-bootstrap";
+import SmallTimeAgo from "../TimeAgo/SmallTimeAgo";
 import {
   getDescriptionFromStatusCode,
   isSuccess,
 } from "../../utils/statusCodes";
 
 interface SubmitToastProps {
+  className: string;
   statusCode: number;
-  onClose?: (event?: React.MouseEvent | React.KeyboardEvent) => void;
+  onCloseCallback: () => void;
 }
 
+const fadeDuration = 5000;
+
 const SubmitToast = (props: SubmitToastProps) => {
-  const { statusCode, onClose } = { ...props };
-  const [description] = useState<string>(() =>
-    getDescriptionFromStatusCode(statusCode),
-  );
-  const [success] = useState<boolean>(() => isSuccess(statusCode));
-  const [className] = useState<string>(
-    success ? "text-success" : "text-danger",
-  );
-  const [heading] = useState<string>(success ? "Success!" : "Error!");
+  const { className, statusCode, onCloseCallback } = { ...props };
+
+  const description = getDescriptionFromStatusCode(statusCode);
+  const success = isSuccess(statusCode);
+  const headingClassName = success ? "text-success" : "text-danger";
+  const heading = success ? "Success!" : "Error!";
+
+  const [show, setShow] = useState<boolean>(true);
+
+  const handleOnClose = () => {
+    setShow(false);
+  };
 
   return (
-    <Toast onClose={onClose}>
-      <Toast.Header>
-        <strong className={`me-auto ${className}`}>{heading}</strong>
-        <SmallTimeAgo />
-      </Toast.Header>
-      <Toast.Body>{description}</Toast.Body>
-    </Toast>
+    <Fade
+      className={className}
+      in={show}
+      timeout={fadeDuration}
+      appear
+      onExited={onCloseCallback}
+    >
+      <div>
+        <Toast show onClose={handleOnClose} animation={false}>
+          <Toast.Header>
+            <strong className={`me-auto ${headingClassName}`}>{heading}</strong>
+            <SmallTimeAgo />
+          </Toast.Header>
+          <Toast.Body>{description}</Toast.Body>
+        </Toast>
+      </div>
+    </Fade>
   );
 };
 
