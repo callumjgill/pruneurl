d3f46e61dba45bd797a40b9cb6fc27f151745c5f
Callum Gill <callum.j.gill@googlemail.com>
2023-07-09 21:34:06 +0100

    fix(buttons): fix spinner size in form buttons
    The spinners are now of the "sm" size so they won't cause the button
    height to increase now.

--- frontend/src/components/buttons/ButtonSpinner.tsx
+++ frontend/src/components/buttons/ButtonSpinner.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "react-bootstrap";
 
 const ButtonSpinner = () => {
   return (
-    <Spinner animation="border" role="status">
+    <Spinner animation="border" role="status" size="sm">
       <span className="visually-hidden">Loading...</span>
     </Spinner>
   );
