722e373a63d1708a3c21c059fa365402758f7f9a
Callum Gill <callum.j.gill@googlemail.com>
2023-07-13 18:09:26 +0100

    feat(frontend): add API error enviroment variables for development

--- frontend/src/middleware/API/DummyAPI.ts
+++ frontend/src/middleware/API/DummyAPI.ts
@@ -1,7 +1,6 @@
 import API from "./API";
 import PruneUrlResult from "./DTOs/PruneUrlResult";
 
-//TODO: add togglable errors
 export default class DummyApi implements API {
   private dummyPrunedUrl: string = `abc`;
 
@@ -9,6 +8,7 @@ export default class DummyApi implements API {
     await this.simulateSubmittingUrlToBackend();
     const result: PruneUrlResult = {
       prunedUrl: !pruneUrl ? this.dummyPrunedUrl : pruneUrl,
+      error: this.getError(),
     };
     return result;
   }
@@ -16,4 +16,22 @@ export default class DummyApi implements API {
   private simulateSubmittingUrlToBackend = (): Promise<void> => {
     return new Promise((resolve) => setTimeout(resolve, 2000));
   };
+
+  private getError(): number | undefined {
+    if (
+      this.returnError() &&
+      process.env.REACT_APP_DUMMY_API_ERRORS_STATUSCODE !== undefined
+    ) {
+      return Number(process.env.REACT_APP_DUMMY_API_ERRORS_STATUSCODE);
+    }
+
+    return undefined;
+  }
+
+  private returnError(): boolean {
+    return (
+      process.env.REACT_APP_DUMMY_API_ERRORS !== undefined &&
+      process.env.REACT_APP_DUMMY_API_ERRORS === "true"
+    );
+  }
 }
