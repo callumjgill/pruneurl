5ada09bd145334430fd98bdde10bc2de9dac10f5
Callum Gill <callum.j.gill@googlemail.com>
2023-07-14 15:47:28 +0100

    refactor(frontend): extend CopyToClipboardButton to use tooltip once text copied

--- frontend/src/components/buttons/CopyToClipboardButton.tsx
+++ frontend/src/components/buttons/CopyToClipboardButton.tsx
@@ -1,6 +1,7 @@
 import { Button } from "react-bootstrap";
 import ButtonSpinner from "./ButtonSpinner";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import CopyToClipboardTooltip from "../tooltips/CopyToClipboardTooltip";
 
 interface CopyToClipboardButtonProps {
   text: string;
@@ -9,18 +10,33 @@ interface CopyToClipboardButtonProps {
 const CopyToClipboardButton = (props: CopyToClipboardButtonProps) => {
   const { text } = { ...props };
 
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
   const [copying, setCopying] = useState<boolean>(false);
+  const [showTooltip, setShowTooltip] = useState<boolean>(false);
 
   const handleClick = async () => {
     setCopying(true);
     await navigator.clipboard.writeText(text);
     setCopying(false);
+    setShowTooltip(true);
   };
 
   return (
-    <Button className="copy-button" variant="secondary" onClick={handleClick}>
-      {copying ? <ButtonSpinner /> : "Copy"}
-    </Button>
+    <>
+      <Button
+        ref={buttonRef}
+        className="copy-button"
+        variant="secondary"
+        onClick={handleClick}
+      >
+        {copying ? <ButtonSpinner /> : "Copy"}
+      </Button>
+      <CopyToClipboardTooltip
+        target={buttonRef.current}
+        show={showTooltip}
+        onTimeout={() => setShowTooltip(false)}
+      />
+    </>
   );
 };
 
