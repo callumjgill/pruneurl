714155bce69213eab387d6fd1a4d9f95900ea74d
Callum Gill <callum.j.gill@googlemail.com>
2023-07-14 15:45:20 +0100

    feat(frontend): add waitAsync function to time utils

--- frontend/src/utils/time.ts
+++ frontend/src/utils/time.ts
@@ -1,1 +1,8 @@
 export const oneMinuteInMilliseconds = 60000;
+export const oneSecondInMilliseconds = 1000;
+
+export const waitAsync = (seconds: number): Promise<void> => {
+  return new Promise((resolve) =>
+    setTimeout(resolve, seconds * oneSecondInMilliseconds),
+  );
+};
