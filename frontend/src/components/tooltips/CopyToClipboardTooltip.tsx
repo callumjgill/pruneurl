efa2d40b202f2e8e8da3cd1ca5014fe56a913b68
Callum Gill <callum.j.gill@googlemail.com>
2023-07-14 15:46:35 +0100

    feat(frontend): add CopyToClipboardTooltip

---
+++ frontend/src/components/tooltips/CopyToClipboardTooltip.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { Overlay, Tooltip } from "react-bootstrap";
+import { waitAsync } from "../../utils/time";
+
+interface CopyToClipboardTooltipProps {
+  target: HTMLElement | null;
+  show?: boolean;
+  onTimeout?: () => void;
+}
+
+const CopyToClipboardTooltip = (props: CopyToClipboardTooltipProps) => {
+  const { target, show, onTimeout } = { ...props };
+
+  const [internalShow, setInternalShow] = useState<boolean>(show ?? false);
+
+  useEffect(() => {
+    setInternalShow(show ?? false);
+    if (show) {
+      waitAsync(5).then(() => setInternalShow(false));
+    }
+  }, [show]);
+
+  return (
+    <Overlay
+      target={target}
+      show={internalShow}
+      placement="right"
+      onExited={onTimeout}
+    >
+      <Tooltip>Copied!</Tooltip>
+    </Overlay>
+  );
+};
+
+export default CopyToClipboardTooltip;
