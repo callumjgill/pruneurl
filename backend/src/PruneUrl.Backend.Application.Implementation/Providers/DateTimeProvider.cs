ef1d54141efcf5c1b6ad6ba1f60f323631753ec9
Callum Gill <callum.j.gill@googlemail.com>
2023-07-15 14:44:04 +0100

    feat(backend): add DateTimeProvider implementation of IDateTimeProvider

---
+++ backend/src/PruneUrl.Backend.Application.Implementation/Providers/DateTimeProvider.cs
@@ -0,0 +1,22 @@
+﻿using PruneUrl.Backend.Application.Interfaces.Providers;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PruneUrl.Backend.Application.Implementation.Providers
+{
+  /// <summary>
+  /// A provider of <see cref="DateTime" /> values.
+  /// </summary>
+  [ExcludeFromCodeCoverage(Justification = "Implements an abstraction to aid with testing, so is itself hard to test!")]
+  public sealed class DateTimeProvider : IDateTimeProvider
+  {
+    #region Public Methods
+
+    /// <inheritdoc cref="IDateTimeProvider.GetNow" />
+    public DateTime GetNow()
+    {
+      return DateTime.Now;
+    }
+
+    #endregion Public Methods
+  }
+}
