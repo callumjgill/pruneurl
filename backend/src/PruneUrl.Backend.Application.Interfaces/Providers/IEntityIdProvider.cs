f0ee06fbca6c195051844f51eb3e6f62a5ef2458
Callum Gill <callum.j.gill@googlemail.com>
2023-07-15 13:27:47 +0100

    feat(backend): add IEntityIdProvider interface

---
+++ backend/src/PruneUrl.Backend.Application.Interfaces/Providers/IEntityIdProvider.cs
@@ -0,0 +1,20 @@
+﻿using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Interfaces.Providers
+{
+  /// <summary>
+  /// Defines a provider for unique identifiers for <see cref="IEntity" />'s.
+  /// </summary>
+  public interface IEntityIdProvider
+  {
+    #region Public Methods
+
+    /// <summary>
+    /// Creates and returns a new unique identifier.
+    /// </summary>
+    /// <returns> A <see cref="string" /> representing the unqiue identifier. </returns>
+    string NewId();
+
+    #endregion Public Methods
+  }
+}
