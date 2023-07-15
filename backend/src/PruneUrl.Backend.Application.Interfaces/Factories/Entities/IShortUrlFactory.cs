44959863ab8a93cbbae2c841014f983d76e428a7
Callum Gill <callum.j.gill@googlemail.com>
2023-07-15 13:13:47 +0100

    feat(backend): add IShortUrlFactory interface

---
+++ backend/src/PruneUrl.Backend.Application.Interfaces/Factories/Entities/IShortUrlFactory.cs
@@ -0,0 +1,26 @@
+﻿using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Interfaces.Factories.Entities
+{
+  /// <summary>
+  /// Defines a factory for creating <see cref="ShortUrl" /> instances.
+  /// </summary>
+  public interface IShortUrlFactory
+  {
+    #region Public Methods
+
+    /// <summary>
+    /// Creates a new <see cref="ShortUrl" /> instance given a <paramref name="longUrl" /> and
+    /// optional <paramref name="shortUrl" />.
+    /// </summary>
+    /// <param name="longUrl"> The long url the <see cref="ShortUrl" /> instance will encapsulate. </param>
+    /// <param name="shortUrl"> Optional short url the end user wishes to use. </param>
+    /// <returns>
+    /// A new <see cref="ShortUrl" /> instance. This will have a new unique ID and not reference an
+    /// existing "short url".
+    /// </returns>
+    ShortUrl Create(string longUrl, string? shortUrl = null);
+
+    #endregion Public Methods
+  }
+}
