a4e71524993ba4f430030c94b700b061e704a434
Callum Gill <callum.j.gill@googlemail.com>
2023-07-15 13:38:39 +0100

    feat(backend): add IShortUrlProvider interface

---
+++ backend/src/PruneUrl.Backend.Application.Interfaces/Providers/IShortUrlProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace PruneUrl.Backend.Application.Interfaces.Providers
+{
+  /// <summary>
+  /// Defines a provider for "short" url's. Implementations will generate and return a short url
+  /// based upon the given long url.
+  /// </summary>
+  public interface IShortUrlProvider
+  {
+    #region Public Methods
+
+    /// <summary>
+    /// Returns a "short" url given a "long" url.
+    /// </summary>
+    /// <param name="longUrl"> The long url to shorten. </param>
+    /// <returns> A shortened url. </returns>
+    string GetShortUrl(string longUrl);
+
+    #endregion Public Methods
+  }
+}
