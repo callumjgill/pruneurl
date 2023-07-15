ee3b8d4ea5d87f2f3933236e16e4f3fac5fe367b
Callum Gill <callum.j.gill@googlemail.com>
2023-07-15 12:17:55 +0100

    feat(backend): add IEntity interface

---
+++ backend/src/PruneUrl.Backend.Domain.Entities/IEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace PruneUrl.Backend.Domain.Entities
+{
+  /// <summary>
+  /// Defines an "entity" which represets a core concept within the backend application. This is a
+  /// pure data structure, and implementations should be immutable. Each entity is like a
+  /// "reference" to a thing which is primarily referenced by its id. If multiple entities of the
+  /// same type with the same id are found in memory, they're refering to the same encapsualted domain.
+  /// </summary>
+  public interface IEntity
+  {
+    #region Public Properties
+
+    /// <summary>
+    /// The unique identifier for the <see cref="IEntity" />.
+    /// </summary>
+    public string Id { get; }
+
+    #endregion Public Properties
+  }
+}
