6d36b764c98db3a7734eb8f99349d28a5f64d490
Callum Gill <callum.j.gill@googlemail.com>
2023-07-16 10:26:44 +0100

    test(backend): add SequenceIdFactoryUnitTests

---
+++ backend/src/PruneUrl.Backend.Application.Implementation.Tests/UnitTests/Factories/Entities/SequenceIdFactoryUnitTests.cs
@@ -0,0 +1,99 @@
+﻿using Moq;
+using NUnit.Framework;
+using PruneUrl.Backend.Application.Implementation.Factories.Entities;
+using PruneUrl.Backend.Application.Interfaces.Providers;
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
+
+namespace PruneUrl.Backend.Application.Implementation.Tests.UnitTests.Factories.Entities
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class SequenceIdFactoryUnitTests
+  {
+    #region Private Fields
+
+    private const int NumberOfIntsToTest = 10000;
+
+    #endregion Private Fields
+
+    #region Public Methods
+
+    [Test]
+    public void CreateTest_Invalid()
+    {
+      var sequenceIdFactory = new SequenceIdFactory(Mock.Of<IEntityIdProvider>());
+      var negativeIntegers = GetRandomRangeOfValues(int.MinValue, 0, NumberOfIntsToTest);
+      var exceptions = new ConcurrentQueue<Exception>();
+      Parallel.ForEach(negativeIntegers, invalidSequenceId =>
+      {
+        try
+        {
+          Assert.That(() => sequenceIdFactory.Create(invalidSequenceId), Throws.TypeOf<ArgumentException>());
+        }
+        catch (Exception ex)
+        {
+          exceptions.Enqueue(ex);
+        }
+      });
+
+      if (!exceptions.IsEmpty)
+      {
+        throw new AggregateException(exceptions);
+      }
+    }
+
+    [Test]
+    public void CreateTest_Valid()
+    {
+      const string testId = "Testing123";
+      var positiveIntegers = GetRandomRangeOfValues(0, int.MaxValue, NumberOfIntsToTest);
+      var exceptions = new ConcurrentQueue<Exception>();
+      Parallel.ForEach(positiveIntegers, sequenceId =>
+      {
+        try
+        {
+          var entityIdProviderMock = new Mock<IEntityIdProvider>();
+
+          entityIdProviderMock.Setup(x => x.NewId()).Returns(testId);
+
+          var sequenceIdFactory = new SequenceIdFactory(entityIdProviderMock.Object);
+          var actualSequenceId = sequenceIdFactory.Create(sequenceId);
+          Assert.Multiple(() =>
+          {
+            Assert.That(actualSequenceId.Id, Is.EqualTo(testId));
+            Assert.That(actualSequenceId.Value, Is.EqualTo(sequenceId));
+          });
+          entityIdProviderMock.Verify(x => x.NewId(), Times.Once);
+        }
+        catch (Exception ex)
+        {
+          exceptions.Enqueue(ex);
+        }
+      });
+
+      if (!exceptions.IsEmpty)
+      {
+        throw new AggregateException(exceptions);
+      }
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    private IEnumerable<int> GetRandomRangeOfValues(int inclusiveMinValue, int exclusiveMaxValue, int count)
+    {
+      var rangeOfInts = new List<int>();
+      while (count > 0)
+      {
+        rangeOfInts.Add(RandomNumberGenerator.GetInt32(inclusiveMinValue, exclusiveMaxValue));
+        count--;
+      }
+
+      return rangeOfInts;
+    }
+
+    #endregion Private Methods
+  }
+}
