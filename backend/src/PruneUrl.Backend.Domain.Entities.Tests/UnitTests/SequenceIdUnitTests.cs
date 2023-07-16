01cddf0433b9b10b3a477cdbe7b5cd92835615be
Callum Gill <callum.j.gill@googlemail.com>
2023-07-16 09:50:23 +0100

    test(backend): add SequenceIdUnitTests

---
+++ backend/src/PruneUrl.Backend.Domain.Entities.Tests/UnitTests/SequenceIdUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.TestHelpers;
+
+namespace PruneUrl.Backend.Domain.Entities.Tests.UnitTests
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class SequenceIdUnitTests
+  {
+    #region Public Methods
+
+    [TestCase("", default(int))]
+    [TestCase("ab0ed3aa-b540-4732-a11f-1d43333a659d", 1076)]
+    [TestCase("This is an id", 432)]
+    public void ConstructorTest(string id, int value)
+    {
+      SequenceId testSequenceId = EntityTestHelper.CreateSequenceId(id, value);
+      Assert.Multiple(() =>
+      {
+        Assert.That(testSequenceId.Id, Is.EqualTo(id));
+        Assert.That(testSequenceId.Value, Is.EqualTo(value));
+      });
+    }
+
+    #endregion Public Methods
+  }
+}
