e1f5c1d30bde270248ee3dae92b4ed082c4c8f58
Callum Gill <callum.j.gill@googlemail.com>
2023-07-16 16:26:27 +0100

    feat(backend): add IDbTransactionFactory interface

---
+++ backend/src/PruneUrl.Backend.Application.Interfaces.Database/DbTransaction/IDbTransactionFactory.cs
@@ -0,0 +1,21 @@
+﻿using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Interfaces.Database.DbTransaction
+{
+  /// <summary>
+  /// Defines a factory for creating <see cref="IDbTransaction{T}" /> instances.
+  /// </summary>
+  public interface IDbTransactionFactory
+  {
+    #region Public Methods
+
+    /// <summary>
+    /// Creates a new <see cref="IDbTransaction{T}" /> instance.
+    /// </summary>
+    /// <typeparam name="T"> The <see cref="IEntity" /> the transaction is concerned with. </typeparam>
+    /// <returns> A new <see cref="IDbTransaction{T}" /> instance. </returns>
+    IDbTransaction<T> Create<T>() where T : IEntity;
+
+    #endregion Public Methods
+  }
+}
