527653867162a14480aa83a1b15c3b04ccf5ecba
Callum Gill <callum.j.gill@googlemail.com>
2023-07-16 23:41:31 +0100

    test(backend): add utility functions for working with FirestoreDb in test enviroment

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Tests/Utilities/TestFirestoreDbHelper.cs
@@ -0,0 +1,75 @@
+﻿using Google.Api.Gax;
+using Google.Cloud.Firestore;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Tests.Utilities
+{
+  /// <summary>
+  /// A helper for working with instances of <see cref="FirestoreDb" /> for testing.
+  /// </summary>
+  internal static class TestFirestoreDbHelper
+  {
+    #region Private Fields
+
+    private const string emulatorEnviromentVariable = "FIRESTORE_EMULATOR_HOST";
+    private const string projectIdEnviromentVariable = "FIRESTORE_EMULATOR_PROJECT_ID";
+
+    #endregion Private Fields
+
+    #region Public Methods
+
+    /// <summary>
+    /// Clears the emulated database for the next test run.
+    /// </summary>
+    /// <returns> A task representing the asynchronous operation of clearing the database. </returns>
+    /// <remarks>
+    /// It is important that tests are run in isolation, so they cannot be ran in parallel with
+    /// other tests which alter the state of the emulator.
+    /// </remarks>
+    public static async Task ClearEmulatedDatabase()
+    {
+      var httpClient = new HttpClient();
+      (string emulatorHost, string projectId) = GetTestFirestoreEnviromentVariables();
+      var requestUri = new Uri($"http://{emulatorHost}/emulator/v1/projects/{projectId}/databases/(default)/documents");
+      using HttpResponseMessage response = await httpClient.DeleteAsync(requestUri);
+      response.EnsureSuccessStatusCode();
+    }
+
+    /// <summary>
+    /// Creates a test <see cref="FirestoreDb" /> instance.
+    /// </summary>
+    /// <returns> The test <see cref="FirestoreDb" /> instance. </returns>
+    public static FirestoreDb GetTestFirestoreDb()
+    {
+      (_, string projectId) = GetTestFirestoreEnviromentVariables();
+      var builder = new FirestoreDbBuilder()
+      {
+        EmulatorDetection = EmulatorDetection.EmulatorOnly,
+        ProjectId = projectId
+      };
+      return builder.Build();
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    private static (string, string) GetTestFirestoreEnviromentVariables()
+    {
+      string? emulatorHost = Environment.GetEnvironmentVariable(emulatorEnviromentVariable);
+      if (emulatorHost == null)
+      {
+        throw new InvalidOperationException($"The enviroment variable '{emulatorEnviromentVariable}' has not been set!");
+      }
+
+      string? emulatorProjectId = Environment.GetEnvironmentVariable(projectIdEnviromentVariable);
+      if (emulatorProjectId == null)
+      {
+        throw new InvalidOperationException($"The enviroment variable '{projectIdEnviromentVariable}' has not been set!");
+      }
+
+      return (emulatorHost, emulatorProjectId);
+    }
+
+    #endregion Private Methods
+  }
+}
