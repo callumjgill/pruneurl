d3feb0d388f90c7484e4f381caaff23f807dd904
Callum Gill <callum.j.gill@googlemail.com>
2023-07-16 16:38:27 +0100

    feat(backend): add CollectionReferenceHelper to firestore infrastructure project

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database/Firestore/Utilities/CollectionReferenceHelper.cs
@@ -0,0 +1,29 @@
+﻿using Google.Cloud.Firestore;
+using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Firestore.Utilities
+{
+  /// <summary>
+  /// A helper class containing functions useful for dealing with <see cref="CollectionReference" />'s.
+  /// </summary>
+  internal static class CollectionReferenceHelper
+  {
+    #region Public Methods
+
+    /// <summary>
+    /// Returns the relative path for a <see cref="CollectionReference" /> given a <typeparamref
+    /// name="T" /> type.
+    /// </summary>
+    /// <typeparam name="T"> The <see cref="IEntity" /> type for the collection of concern. </typeparam>
+    /// <returns>
+    /// The relative path for a <see cref="CollectionReference" /> holding documents concerning the
+    /// <typeparamref name="T" /> type.
+    /// </returns>
+    public static string GetCollectionPath<T>() where T : IEntity
+    {
+      return $"{typeof(T).Name}s";
+    }
+
+    #endregion Public Methods
+  }
+}
