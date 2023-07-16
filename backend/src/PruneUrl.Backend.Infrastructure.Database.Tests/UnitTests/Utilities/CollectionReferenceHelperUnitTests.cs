6107a3e8e61c34073454084cce765542c2d48052
Callum Gill <callum.j.gill@googlemail.com>
2023-07-16 22:55:59 +0100

    test(backend): add unit tests for CollectionReferenceHelper

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Tests/UnitTests/Utilities/CollectionReferenceHelperUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Domain.Entities;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.Utilities;
+using PruneUrl.Backend.TestHelpers;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Tests.UnitTests.Utilities
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class CollectionReferenceHelperUnitTests
+  {
+    #region Public Methods
+
+    [TestCase<IEntity>("IEntitys")]
+    [TestCase<ShortUrl>("ShortUrls")]
+    [TestCase<SequenceId>("SequenceIds")]
+    public void GetCollectionPathTest<T>(string expectedResult) where T : IEntity
+    {
+      Assert.That(CollectionReferenceHelper.GetCollectionPath<T>(), Is.EqualTo(expectedResult));
+    }
+
+    #endregion Public Methods
+  }
+}
