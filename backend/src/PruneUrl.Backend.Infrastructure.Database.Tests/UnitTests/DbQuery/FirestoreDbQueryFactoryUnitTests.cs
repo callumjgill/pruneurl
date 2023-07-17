16376f387c550c0d7149bd1850a816dc1d5c8e26
Callum Gill <callum.j.gill@googlemail.com>
2023-07-17 13:31:12 +0100

    test(backend): add unit tests for FirestoreDbQueryFactory

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Tests/UnitTests/DbQuery/FirestoreDbQueryFactoryUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using Google.Cloud.Firestore;
+using NUnit.Framework;
+using PruneUrl.Backend.Application.Interfaces.Database.DbQuery;
+using PruneUrl.Backend.Domain.Entities;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.DbQuery;
+using PruneUrl.Backend.Infrastructure.Database.Tests.Utilities;
+using PruneUrl.Backend.TestHelpers;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Tests.UnitTests.DbQuery
+{
+  [TestFixture]
+  [Parallelizable]
+  [Description("These tests don't require communicating with the FirestoreDb emulator, so they can be ran in parallel with each other and other test fixtures.")]
+  public sealed class FirestoreDbQueryFactoryUnitTests
+  {
+    #region Public Methods
+
+    [TestCase<IEntity>]
+    [TestCase<ShortUrl>]
+    [TestCase<SequenceId>]
+    public void CreateTest<T>() where T : IEntity
+    {
+      FirestoreDb testFirestoreDb = TestFirestoreDbHelper.GetTestFirestoreDb();
+      var factory = new FirestoreDbQueryFactory(testFirestoreDb);
+      IDbQuery<T> actualQuery = factory.Create<T>();
+      Assert.That(actualQuery, Is.TypeOf<FirestoreDbQuery<T>>());
+    }
+
+    #endregion Public Methods
+  }
+}
