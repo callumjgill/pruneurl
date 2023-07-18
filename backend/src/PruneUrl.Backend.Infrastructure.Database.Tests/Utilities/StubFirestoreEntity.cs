cd652466800d80ef3ed001c8534994348d5f909d
Callum Gill <callum.j.gill@googlemail.com>
2023-07-18 14:11:08 +0100

    test(backend): add StubFirestoreEntity for testing firestore DB code

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Tests/Utilities/StubFirestoreEntity.cs
@@ -0,0 +1,46 @@
+﻿using Google.Cloud.Firestore;
+using PruneUrl.Backend.Domain.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Tests.Utilities
+{
+  /// <summary>
+  /// A test, or "stub", version of a "firestore" entity.
+  /// </summary>
+  [FirestoreData]
+  internal sealed class StubFirestoreEntity : IEntity
+  {
+    #region Public Constructors
+
+    /// <summary>
+    /// Constructor used by firestore in deserialization
+    /// </summary>
+    public StubFirestoreEntity()
+    {
+    }
+
+    #endregion Public Constructors
+
+    #region Internal Constructors
+
+    /// <summary>
+    /// Constructor for creating a new instance
+    /// </summary>
+    /// <param name="id"> The id of the entity </param>
+    internal StubFirestoreEntity(string id)
+    {
+      Id = id;
+    }
+
+    #endregion Internal Constructors
+
+    #region Public Properties
+
+    /// <inheritdoc cref="IEntity.Id" />
+    [FirestoreDocumentId]
+    [AllowNull]
+    public string Id { get; set; }
+
+    #endregion Public Properties
+  }
+}
