9fe5db7d880ffb253ae07f492b216a38e98950c2
Callum Gill <callum.j.gill@googlemail.com>
2023-07-19 22:31:48 +0100

    feat(backend): add adapters for IDbQuery and IDbTransaction for DTO types to core types

--- backend/src/PruneUrl.Backend.Infrastructure.Database.Tests/UnitTests/DbTransaction/FirestoreDbTransactionFactoryUnitTests.cs
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Tests/UnitTests/DbTransaction/FirestoreDbTransactionFactoryUnitTests.cs
@@ -1,10 +1,13 @@
-﻿using Google.Cloud.Firestore;
+﻿using AutoMapper;
+using Google.Cloud.Firestore;
+using Moq;
 using NUnit.Framework;
 using PruneUrl.Backend.Application.Interfaces.Database.DbTransaction;
 using PruneUrl.Backend.Domain.Entities;
 using PruneUrl.Backend.Infrastructure.Database.Firestore.DbTransaction;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.DTOs;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.Exceptions;
 using PruneUrl.Backend.Infrastructure.Database.Tests.Utilities;
-using PruneUrl.Backend.TestHelpers;
 
 namespace PruneUrl.Backend.Infrastructure.Database.Tests.UnitTests.DbTransaction
 {
@@ -15,17 +18,40 @@ namespace PruneUrl.Backend.Infrastructure.Database.Tests.UnitTests.DbTransaction
   {
     #region Public Methods
 
-    [TestCase<IEntity>]
-    [TestCase<ShortUrl>]
-    [TestCase<SequenceId>]
-    public void CreateTest<T>() where T : IEntity
+    [Test]
+    public void CreateTest_Invalid()
     {
       FirestoreDb testFirestoreDb = TestFirestoreDbHelper.GetTestFirestoreDb();
-      var factory = new FirestoreDbTransactionFactory(testFirestoreDb);
-      IDbTransaction<T> actualTransaction = factory.Create<T>();
-      Assert.That(actualTransaction, Is.TypeOf<FirestoreDbTransaction<T>>());
+      var factory = new FirestoreDbTransactionFactory(testFirestoreDb, Mock.Of<IMapper>());
+      Assert.That(factory.Create<IEntity>, Throws.TypeOf<InvalidEntityTypeMapException>().With.Message.EqualTo($"No mapping exists for the type {typeof(IEntity)}!"));
+    }
+
+    [Test]
+    public void CreateTest_Valid_SequenceId()
+    {
+      CreateTest<SequenceId, SequenceIdDTO>();
+    }
+
+    [Test]
+    public void CreateTest_Valid_ShortUrl()
+    {
+      CreateTest<ShortUrl, ShortUrlDTO>();
     }
 
     #endregion Public Methods
+
+    #region Private Methods
+
+    private void CreateTest<TEntity, TFirestoreEntity>()
+      where TEntity : IEntity
+      where TFirestoreEntity : FirestoreEntityDTO
+    {
+      FirestoreDb testFirestoreDb = TestFirestoreDbHelper.GetTestFirestoreDb();
+      var factory = new FirestoreDbTransactionFactory(testFirestoreDb, Mock.Of<IMapper>());
+      IDbTransaction<TEntity> actualTransaction = factory.Create<TEntity>();
+      Assert.That(actualTransaction, Is.TypeOf<FirestoreDbTransactionAdapter<TEntity, TFirestoreEntity>>());
+    }
+
+    #endregion Private Methods
   }
 }
---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database/DbTransaction/FirestoreDbTransactionAdapter.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using PruneUrl.Backend.Application.Interfaces.Database.DbTransaction;
+using PruneUrl.Backend.Domain.Entities;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.DTOs;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Firestore.DbTransaction
+{
+  /// <summary>
+  /// An adapter which converts a <typeparamref name="TEntity" /> input into a <typeparamref
+  /// name="TFirestoreEntity" /> that can be used for writing to the firestore database via the SDK.
+  /// </summary>
+  /// <typeparam name="TEntity"> The <see cref="IEntity" /> the app uses. </typeparam>
+  /// <typeparam name="TFirestoreEntity">
+  /// The <see cref="FirestoreEntityDTO" /> the internal <see cref="IDbTransaction{T}" /> uses.
+  /// </typeparam>
+  internal class FirestoreDbTransactionAdapter<TEntity, TFirestoreEntity> : IDbTransaction<TEntity>
+    where TEntity : IEntity
+    where TFirestoreEntity : FirestoreEntityDTO
+  {
+    #region Private Fields
+
+    private readonly IMapper mapper;
+    private IDbTransaction<TFirestoreEntity> firestoreDbTransaction;
+
+    #endregion Private Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="FirestoreDbTransactionAdapter{TEntity,
+    /// TFirestoreEntity}" /> class.
+    /// </summary>
+    /// <param name="firestoreDbTransaction"> The adaptee <see cref="IDbTransaction{T}" />. </param>
+    /// <param name="mapper">
+    /// The <see cref="IMapper" /> service for converting between the <see cref="FirestoreEntityDTO"
+    /// />'s and the core <see cref="IEntity" />'s.
+    /// </param>
+    public FirestoreDbTransactionAdapter(IDbTransaction<TFirestoreEntity> firestoreDbTransaction, IMapper mapper)
+    {
+      this.firestoreDbTransaction = firestoreDbTransaction;
+      this.mapper = mapper;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Methods
+
+    /// <inheritdoc cref="IDbTransaction{T}.CommitAsync(CancellationToken)" />
+    public Task CommitAsync(CancellationToken cancellationToken = default)
+    {
+      return firestoreDbTransaction.CommitAsync(cancellationToken);
+    }
+
+    /// <inheritdoc cref="IDbTransaction{T}.Create(T)" />
+    public IDbTransaction<TEntity> Create(TEntity entity)
+    {
+      TFirestoreEntity firestoreEntity = mapper.Map<TEntity, TFirestoreEntity>(entity);
+      firestoreDbTransaction = firestoreDbTransaction.Create(firestoreEntity);
+      return this;
+    }
+
+    /// <inheritdoc cref="IDbTransaction{T}.Delete(string)" />
+    public IDbTransaction<TEntity> Delete(string id)
+    {
+      firestoreDbTransaction = firestoreDbTransaction.Delete(id);
+      return this;
+    }
+
+    #endregion Public Methods
+  }
+}
