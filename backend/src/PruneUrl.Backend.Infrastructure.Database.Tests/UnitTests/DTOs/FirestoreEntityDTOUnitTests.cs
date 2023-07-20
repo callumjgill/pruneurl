030605e4314189badfc68c4c7973830b66c9d4da
Callum Gill <callum.j.gill@googlemail.com>
2023-07-20 18:07:03 +0100

    test(backend): add unit tests for FirestoreEntityDTO's

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Tests/UnitTests/DTOs/FirestoreEntityDTOUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.DTOs;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Firestore.Tests.UnitTests.DTOs
+{
+  internal abstract class FirestoreEntityDTOUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void IdTest()
+    {
+      string testId = Guid.NewGuid().ToString();
+      PropertyTest(null, testId, (entityDTO) => entityDTO.Id, (entityDTO, newValue) => entityDTO.Id = newValue);
+    }
+
+    #endregion Public Methods
+
+    #region Protected Methods
+
+    protected abstract FirestoreEntityDTO CreateDTO();
+
+    protected void PropertyTest<T>(T? defaultValue, T newValue, Func<FirestoreEntityDTO, T> getProperty, Action<FirestoreEntityDTO, T> setProperty)
+    {
+      FirestoreEntityDTO entityDTO = CreateDTO();
+      Assert.That(getProperty(entityDTO), Is.EqualTo(defaultValue));
+      setProperty(entityDTO, newValue);
+      Assert.That(getProperty(entityDTO), Is.EqualTo(newValue));
+    }
+
+    #endregion Protected Methods
+  }
+}
