5b061088a8022cade0bf6379b1976179c971af37
Callum Gill <callum.j.gill@googlemail.com>
2023-07-20 17:21:04 +0100

    test(backend): add unit tests for the FirestoreDTOProfile

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Tests/UnitTests/Mapping/FirestoreDTOProfileUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using NUnit.Framework;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.Mapping;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Firestore.Tests.UnitTests.Mapping
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class FirestoreDTOProfileUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void ConstructorTest_MappingsAreValid()
+    {
+      var configuration = new MapperConfiguration(cfg =>
+      {
+        cfg.AddProfile<FirestoreDTOProfile>();
+      });
+      Assert.That(configuration.AssertConfigurationIsValid, Throws.Nothing);
+    }
+
+    #endregion Public Methods
+  }
+}
