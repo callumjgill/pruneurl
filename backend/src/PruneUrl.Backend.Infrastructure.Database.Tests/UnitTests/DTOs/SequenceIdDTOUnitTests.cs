1e6a37da3cc77c12dd4611eee0eb54e344f3c391
Callum Gill <callum.j.gill@googlemail.com>
2023-07-20 18:07:03 +0100

    test(backend): add unit tests for FirestoreEntityDTO's

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Tests/UnitTests/DTOs/SequenceIdDTOUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.DTOs;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Firestore.Tests.UnitTests.DTOs
+{
+  [TestFixture]
+  [Parallelizable]
+  internal sealed class SequenceIdDTOUnitTests : FirestoreEntityDTOUnitTests
+  {
+    #region Public Methods
+
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(4)]
+    [TestCase(8)]
+    [TestCase(16)]
+    [TestCase(32)]
+    [TestCase(64)]
+    [TestCase(128)]
+    [TestCase(256)]
+    public void ValueTest(int testValue)
+    {
+      PropertyTest(null, testValue, (entityDTO) => ((SequenceIdDTO)entityDTO).Value, (entityDTO, newValue) => ((SequenceIdDTO)entityDTO).Value = newValue);
+    }
+
+    #endregion Public Methods
+
+    #region Protected Methods
+
+    protected override FirestoreEntityDTO CreateDTO()
+    {
+      return new SequenceIdDTO();
+    }
+
+    #endregion Protected Methods
+  }
+}
