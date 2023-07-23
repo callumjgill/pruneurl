2596cbc836cb1c622fed09ee138f5f0785fc9449
Callum Gill <callum.j.gill@googlemail.com>
2023-07-23 12:56:54 +0100

    feat(backend): add IDbTransaction interface & Firebase implementations for read-write combinations

---
+++ backend/src/PruneUrl.Backend.Application.Interfaces.Database/Requests/IDbTransaction.cs
@@ -0,0 +1,16 @@
+﻿using PruneUrl.Backend.Application.Interfaces.Database.Operations.Read;
+using PruneUrl.Backend.Application.Interfaces.Database.Operations.Write;
+using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Interfaces.Database.Requests
+{
+  /// <summary>
+  /// Defines a "transaction" which is a "batch" of read and write operations which can be performed
+  /// on the database. The read operations can only be performed BEFORE the write operations. This
+  /// is useful for situations where you need to read a value to perform a corresponding write operation.
+  /// </summary>
+  /// <typeparam name="T"> The <see cref="IEntity" /> the transaction is concerned with. </typeparam>
+  public interface IDbTransaction<T> : IDbGetByIdOperation<T>, IDbUpdateOperation<T> where T : IEntity
+  {
+  }
+}
---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Firestore/Requests/FirestoreDbTransactionAdapter.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using PruneUrl.Backend.Application.Interfaces.Database.Operations.Read;
+using PruneUrl.Backend.Application.Interfaces.Database.Operations.Write;
+using PruneUrl.Backend.Application.Interfaces.Database.Requests;
+using PruneUrl.Backend.Domain.Entities;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.DTOs;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Firestore.Requests
+{
+  /// <summary>
+  /// An adapter for <see cref="IDbTransaction{T}" /> which converts a <typeparamref name="TEntity"
+  /// /> input into a <typeparamref name="TFirestoreEntity" /> that can be used for writing to the
+  /// firestore database via the SDK.
+  /// </summary>
+  /// <typeparam name="TEntity"> The <see cref="IEntity" /> the app uses. </typeparam>
+  /// <typeparam name="TFirestoreEntity">
+  /// The <see cref="FirestoreEntityDTO" /> the internal <see cref="IDbTransaction{T}" /> uses.
+  /// </typeparam>
+  internal sealed class FirestoreDbTransactionAdapter<TEntity, TFirestoreEntity> : IDbTransaction<TEntity>
+    where TEntity : IEntity
+    where TFirestoreEntity : FirestoreEntityDTO
+  {
+    #region Private Fields
+
+    private readonly IDbTransaction<TFirestoreEntity> firestoreDbTransaction;
+    private readonly IMapper mapper;
+
+    #endregion Private Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="FirestoreDbTransactionAdapter{TEntity,
+    /// TFirestoreEntity}" /> class.
+    /// </summary>
+    /// <param name="firestoreDbTransaction"> The adaptee <see cref="IDbTransaction{T}" />. </param>
+    /// <param name="mapper">
+    /// The <see cref="IMapper" /> service for converting between the <see cref="FirestoreEntityDTO"
+    /// />'s and the core <see cref="IEntity" />'s.
+    /// </param>
+    public FirestoreDbTransactionAdapter(IDbTransaction<TFirestoreEntity> firestoreDbTransaction, IMapper mapper)
+    {
+      this.firestoreDbTransaction = firestoreDbTransaction;
+      this.mapper = mapper;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Methods
+
+    /// <inheritdoc cref="IDbGetByIdOperation{T}.GetByIdAsync(string, CancellationToken)" />
+    public async Task<TEntity?> GetByIdAsync(string id, CancellationToken cancellationToken = default)
+    {
+      TFirestoreEntity? firestoreEntity = await firestoreDbTransaction.GetByIdAsync(id, cancellationToken);
+      return firestoreEntity != null ? mapper.Map<TFirestoreEntity, TEntity>(firestoreEntity) : default;
+    }
+
+    /// <inheritdoc cref="IDbUpdateOperation{T}.Update(T)" />
+    public void Update(TEntity entity)
+    {
+      TFirestoreEntity firestoreEntity = mapper.Map<TEntity, TFirestoreEntity>(entity);
+      firestoreDbTransaction.Update(firestoreEntity);
+    }
+
+    #endregion Public Methods
+  }
+}
