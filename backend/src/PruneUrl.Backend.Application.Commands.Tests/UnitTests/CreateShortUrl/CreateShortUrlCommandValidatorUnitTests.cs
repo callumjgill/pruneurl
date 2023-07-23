4bae4f3eb2e269a4c0d5639c655a0aeca032ff9d
Callum Gill <callum.j.gill@googlemail.com>
2023-07-23 19:18:13 +0100

    feat(backend): add CreateShortUrlCommand use case classes

---
+++ backend/src/PruneUrl.Backend.Application.Commands.Tests/UnitTests/CreateShortUrl/CreateShortUrlCommandValidatorUnitTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation.TestHelper;
+using NUnit.Framework;
+using PruneUrl.Backend.Application.Commands.CreateShortUrl;
+
+namespace PruneUrl.Backend.Application.Commands.Tests.UnitTests.CreateShortUrl
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class CreateShortUrlCommandValidatorUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void ValidateTest_InvalidProperties()
+    {
+      string testLongUrl = "testing123";
+      int testSequenceId = -6124323;
+      var command = new CreateShortUrlCommand(testLongUrl, testSequenceId);
+      var validator = new CreateShortUrlCommandValidator();
+
+      TestValidationResult<CreateShortUrlCommand> result = validator.TestValidate(command);
+      result.ShouldHaveValidationErrorFor(cmd => cmd.LongUrl);
+      result.ShouldHaveValidationErrorFor(cmd => cmd.SequenceId);
+    }
+
+    [Test]
+    public void ValidateTest_ValidProperties()
+    {
+      string testLongUrl = "https://www.youtube.com";
+      int testSequenceId = 6124323;
+      var command = new CreateShortUrlCommand(testLongUrl, testSequenceId);
+      var validator = new CreateShortUrlCommandValidator();
+
+      TestValidationResult<CreateShortUrlCommand> result = validator.TestValidate(command);
+      result.ShouldNotHaveValidationErrorFor(cmd => cmd.LongUrl);
+      result.ShouldNotHaveValidationErrorFor(cmd => cmd.SequenceId);
+    }
+
+    #endregion Public Methods
+  }
+}
