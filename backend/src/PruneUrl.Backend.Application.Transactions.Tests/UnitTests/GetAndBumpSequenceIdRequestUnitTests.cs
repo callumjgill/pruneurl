05ca5106bae229c257a601d8c654d932c733789e
Callum Gill <callum.j.gill@googlemail.com>
2023-07-23 15:47:33 +0100

    test(backend): add tests for the GetAndBumpSequenceIdRequest transaction classes

---
+++ backend/src/PruneUrl.Backend.Application.Transactions.Tests/UnitTests/GetAndBumpSequenceIdRequestUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using NUnit.Framework;
+using PruneUrl.Backend.Application.Transactions.GetAndBumpSequenceId;
+
+namespace PruneUrl.Backend.Transactions.Tests.UnitTests
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class GetAndBumpSequenceIdRequestUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void ConstructorTest()
+    {
+      var request = new GetAndBumpSequenceIdRequest();
+      Assert.That(request is IRequest<GetAndBumpSequenceIdResponse>, Is.True);
+    }
+
+    #endregion Public Methods
+  }
+}
---
+++ backend/src/PruneUrl.Backend.Application.Transactions.Tests/UnitTests/GetAndBumpSequenceIdResponseUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Application.Transactions.GetAndBumpSequenceId;
+using PruneUrl.Backend.TestHelpers;
+
+namespace PruneUrl.Backend.Transactions.Tests.UnitTests
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class GetAndBumpSequenceIdResponseUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void ConstructorTest()
+    {
+      var sequenceId = EntityTestHelper.CreateSequenceId();
+      var response = new GetAndBumpSequenceIdResponse(sequenceId);
+      Assert.That(response.SequenceId, Is.EqualTo(sequenceId));
+    }
+
+    #endregion Public Methods
+  }
+}
