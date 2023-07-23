77156e4701232c1a32721e60a2c8bd363fbab7dc
Callum Gill <callum.j.gill@googlemail.com>
2023-07-23 13:00:20 +0100

    test(backend): add unit tests for the Firestore IDbTransactionProvider related implementations

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Firestore.Tests/UnitTests/Requests/FirestoreDbTransactionFactoryUnitTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Google.Cloud.Firestore;
+using Moq;
+using NUnit.Framework;
+using PruneUrl.Backend.Application.Interfaces.Database.Requests;
+using PruneUrl.Backend.Domain.Entities;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.DTOs;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.Exceptions;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.Requests;
+using PruneUrl.Backend.Infrastructure.Database.Tests.Utilities;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Firestore.Tests.UnitTests.Requests
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class FirestoreDbTransactionFactoryUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void CreateTest_Invalid()
+    {
+      FirestoreDb testFirestoreDb = TestFirestoreDbHelper.GetTestFirestoreDb();
+      var factory = new FirestoreDbWriteBatchFactory(testFirestoreDb, Mock.Of<IMapper>());
+      Assert.That(factory.Create<IEntity>, Throws.TypeOf<InvalidEntityTypeMapException>().With.Message.EqualTo($"No mapping exists for the type {typeof(IEntity)}!"));
+    }
+
+    [Test]
+    public Task CreateTest_Valid_SequenceId()
+    {
+      return CreateTest<SequenceId, SequenceIdDTO>();
+    }
+
+    [Test]
+    public Task CreateTest_Valid_ShortUrl()
+    {
+      return CreateTest<ShortUrl, ShortUrlDTO>();
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    private async Task CreateTest<TEntity, TFirestoreEntity>()
+      where TEntity : IEntity
+      where TFirestoreEntity : FirestoreEntityDTO
+    {
+      FirestoreDb testFirestoreDb = TestFirestoreDbHelper.GetTestFirestoreDb();
+      var factory = new FirestoreDbTransactionFactory(testFirestoreDb, Mock.Of<IMapper>());
+      await testFirestoreDb.RunTransactionAsync(transaction =>
+      {
+        IDbTransaction<TEntity> actualDbTransaction = factory.Create<TEntity>(transaction);
+        Assert.That(actualDbTransaction, Is.TypeOf<FirestoreDbTransactionAdapter<TEntity, TFirestoreEntity>>());
+        return Task.CompletedTask;
+      });
+    }
+
+    #endregion Private Methods
+  }
+}
