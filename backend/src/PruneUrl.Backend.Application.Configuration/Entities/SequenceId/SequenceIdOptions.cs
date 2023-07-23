0796fa7b3655d54a8210b3e4c39e53685afa02f8
Callum Gill <callum.j.gill@googlemail.com>
2023-07-23 13:00:38 +0100

    feat(backend): add transaction request use case for retrieving and bumping a sequence id

---
+++ backend/src/PruneUrl.Backend.Application.Configuration/Entities/SequenceId/SequenceIdOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace PruneUrl.Backend.Application.Configuration.Entities.SequenceId
+{
+  /// <summary>
+  /// A configuration DTO intended to be used in the options pattern. This is injected into classes
+  /// via the options pattern. The configuration is specific to the "sequence id" entity.
+  /// </summary>
+  public sealed class SequenceIdOptions
+  {
+    #region Public Properties
+
+    /// <summary>
+    /// The unique identifier used for retrieving a sequence id entity from the database.
+    /// </summary>
+    public string Id { get; set; } = string.Empty;
+
+    #endregion Public Properties
+  }
+}
---
+++ backend/src/PruneUrl.Backend.Application.Transactions/GetAndBumpSequenceId/GetAndBumpSequenceIdResponse.cs
@@ -0,0 +1,12 @@
+﻿using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Transactions.GetAndBumpSequenceId
+{
+  /// <summary>
+  /// The immutable response to a <see cref="GetAndBumpSequenceIdRequest" />.
+  /// </summary>
+  /// <param name="SequenceId"> The <see cref="SequenceId" /> instance found. </param>
+  public sealed record GetAndBumpSequenceIdResponse(SequenceId SequenceId)
+  {
+  }
+}
