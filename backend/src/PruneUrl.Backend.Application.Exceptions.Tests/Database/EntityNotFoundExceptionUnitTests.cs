c01d58ba333d57c10269b5252da1b919cc55bb34
Callum Gill <callum.j.gill@googlemail.com>
2023-07-23 16:05:30 +0100

    test(backend): add unit tests for EntityNotFoundException in application layer

---
+++ backend/src/PruneUrl.Backend.Application.Exceptions.Tests/Database/EntityNotFoundExceptionUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Application.Exceptions.Database;
+
+namespace PruneUrl.Backend.Application.Exceptions.Tests.Database
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class EntityNotFoundExceptionUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void MessageTest()
+    {
+      Type dummyType = typeof(string);
+      string testId = Guid.NewGuid().ToString();
+      var exception = new EntityNotFoundException(dummyType, testId);
+      Assert.That(exception.Message, Is.EqualTo($"Entity of type {dummyType} with id {testId} was not found!"));
+    }
+
+    #endregion Public Methods
+  }
+}
