4ebd822af86eb3e124552d5c28cf9691d9385595
Callum Gill <callum.j.gill@googlemail.com>
2023-07-23 12:32:49 +0100

    feat(backend): add IDbUpdateOperation interface

---
+++ backend/src/PruneUrl.Backend.Application.Interfaces.Database/Operations/Write/IDbUpdateOperation.cs
@@ -0,0 +1,24 @@
+﻿using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Interfaces.Database.Operations.Write
+{
+  /// <summary>
+  /// Defines an operation for updating a <typeparamref name="T" /> in the underlying database.
+  /// </summary>
+  /// <typeparam name="T"> The <see cref="IEntity" /> the operation is concerned with. </typeparam>
+  public interface IDbUpdateOperation<T> where T : IEntity
+  {
+    #region Public Methods
+
+    /// <summary>
+    /// Performs the operation of updating a <typeparamref name="T" />.
+    /// </summary>
+    /// <param name="entity"> The <typeparamref name="T" /> entity to create. </param>
+    /// <remarks>
+    /// It is up to the implementation to either commit this operation immediately or perform it later.
+    /// </remarks>
+    void Update(T entity);
+
+    #endregion Public Methods
+  }
+}
