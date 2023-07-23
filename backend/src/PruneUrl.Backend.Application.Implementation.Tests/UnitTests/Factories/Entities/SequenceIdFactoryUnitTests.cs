41db44d5083bcf9797c0c6b352202d41947d87fb
Callum Gill <callum.j.gill@googlemail.com>
2023-07-23 13:00:30 +0100

    refactor(backend): add CreateFromExisting method to ISequenceIdFactory

--- backend/src/PruneUrl.Backend.Application.Implementation.Tests/UnitTests/Factories/Entities/SequenceIdFactoryUnitTests.cs
+++ backend/src/PruneUrl.Backend.Application.Implementation.Tests/UnitTests/Factories/Entities/SequenceIdFactoryUnitTests.cs
@@ -1,7 +1,9 @@
 ﻿using Moq;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using PruneUrl.Backend.Application.Implementation.Factories.Entities;
 using PruneUrl.Backend.Application.Interfaces.Providers;
+using PruneUrl.Backend.TestHelpers;
 using System.Collections.Concurrent;
 using System.Security.Cryptography;
 
@@ -18,6 +20,68 @@ namespace PruneUrl.Backend.Application.Implementation.Tests.UnitTests.Factories.
     #endregion Private Fields
 
     #region Public Methods
+
+    [Test]
+    public void CreateFromExistingTest_Invalid()
+    {
+      const string testId = "Testing123";
+      var existingSequenceId = EntityTestHelper.CreateSequenceId(testId);
+      var sequenceIdFactory = new SequenceIdFactory(Mock.Of<IEntityIdProvider>());
+      var negativeIntegers = GetRandomRangeOfValues(int.MinValue, 0, NumberOfIntsToTest);
+      var exceptions = new ConcurrentQueue<Exception>();
+      Parallel.ForEach(negativeIntegers, invalidSequenceId =>
+      {
+        try
+        {
+          Assert.That(() => sequenceIdFactory.CreateFromExisting(existingSequenceId, invalidSequenceId), Throws.TypeOf<ArgumentException>());
+        }
+        catch (Exception ex)
+        {
+          exceptions.Enqueue(ex);
+        }
+      });
+
+      if (!exceptions.IsEmpty)
+      {
+        throw new AggregateException(exceptions);
+      }
+    }
+
+    [Test]
+    public void CreateFromExistingTest_Valid()
+    {
+      const string testId = "Testing123";
+      var existingSequenceId = EntityTestHelper.CreateSequenceId(testId);
+      var positiveIntegers = GetRandomRangeOfValues(0, int.MaxValue, NumberOfIntsToTest);
+      var exceptions = new ConcurrentQueue<Exception>();
+      Parallel.ForEach(positiveIntegers, sequenceId =>
+      {
+        try
+        {
+          var entityIdProviderMock = new Mock<IEntityIdProvider>();
+
+          entityIdProviderMock.Setup(x => x.NewId()).Returns(testId);
+
+          var sequenceIdFactory = new SequenceIdFactory(entityIdProviderMock.Object);
+          var actualSequenceId = sequenceIdFactory.CreateFromExisting(existingSequenceId, sequenceId);
+          Assert.Multiple(() =>
+          {
+            Assert.That(actualSequenceId.Id, Is.EqualTo(testId));
+            Assert.That(actualSequenceId.Value, Is.EqualTo(sequenceId));
+          });
+          entityIdProviderMock.Verify(x => x.NewId(), Times.Never);
+        }
+        catch (Exception ex)
+        {
+          exceptions.Enqueue(ex);
+        }
+      });
+
+      if (!exceptions.IsEmpty)
+      {
+        throw new AggregateException(exceptions);
+      }
+    }
 
     [Test]
     public void CreateTest_Invalid()
