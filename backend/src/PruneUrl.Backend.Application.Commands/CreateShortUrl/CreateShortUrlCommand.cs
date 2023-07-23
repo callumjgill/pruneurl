490ee5359de308fe57c1a69312e17eca85045a15
Callum Gill <callum.j.gill@googlemail.com>
2023-07-23 17:42:43 +0100

    feat(backend): add CreateShortUrlCommand use case

---
+++ backend/src/PruneUrl.Backend.Application.Commands/CreateShortUrl/CreateShortUrlCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Commands.CreateShortUrl
+{
+  /// <summary>
+  /// An immutable command for creating a <see cref="ShortUrl" />
+  /// </summary>
+  /// <param name="LongUrl">
+  /// The "long" url, which is the original url the shorter url will allow redirection to.
+  /// </param>
+  /// <param name="SequenceId">
+  /// The integer "sequence id" which will be used to create the short url the end user will use.
+  /// </param>
+  public sealed record CreateShortUrlCommand(string LongUrl, int SequenceId) : IRequest
+  {
+  }
+}
---
+++ backend/src/PruneUrl.Backend.Application.Commands/CreateShortUrl/CreateShortUrlCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+
+namespace PruneUrl.Backend.Application.Commands.CreateShortUrl
+{
+  /// <summary>
+  /// The validator for the <see cref="CreateShortUrlCommand" /> request.
+  /// </summary>
+  public sealed class CreateShortUrlCommandValidator : AbstractValidator<CreateShortUrlCommand>
+  {
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="CreateShortUrlCommandValidator" /> class.
+    /// </summary>
+    public CreateShortUrlCommandValidator()
+    {
+      RuleFor(command => command.SequenceId).GreaterThanOrEqualTo(0);
+      RuleFor(command => command.LongUrl).Must(BeUrl).When(command => !string.IsNullOrWhiteSpace(command.LongUrl));
+    }
+
+    #endregion Public Constructors
+
+    #region Private Methods
+
+    private bool BeUrl(string url)
+    {
+      return Uri.TryCreate(url, UriKind.Absolute, out _);
+    }
+
+    #endregion Private Methods
+  }
+}
