a0420ae6ff508e89ac64042a024376df1f6803f0
Callum Gill <callum.j.gill@googlemail.com>
2023-07-24 16:07:46 +0100

    feat(backend): add MediatR request decorator for validation

---
+++ backend/src/PruneUrl.Backend.Application.Requests/Decorators/ValidateRequestHandlerDecorator.cs
@@ -0,0 +1,94 @@
+﻿using FluentValidation;
+using MediatR;
+using PruneUrl.Backend.Application.Requests.Extensions;
+
+namespace PruneUrl.Backend.Application.Requests.Decorators
+{
+  /// <summary>
+  /// A decorator which provides a validation as part of a request pipeline. This should be the
+  /// first in the pipeline, or at the very least before the actual request handler.
+  /// </summary>
+  /// <typeparam name="TRequest"> The request. </typeparam>
+  public sealed class ValidateRequestHandlerDecorator<TRequest> : IRequestHandler<TRequest> where TRequest : IRequest
+  {
+    #region Private Fields
+
+    private readonly IRequestHandler<TRequest> requestHandlerToDecorate;
+    private readonly IValidator<TRequest> validator;
+
+    #endregion Private Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="ValidateRequestHandlerDecorator{TRequest}" /> class.
+    /// </summary>
+    /// <param name="requestHandlerToDecorate">
+    /// The <see cref="IRequestHandler{TRequest, TResponse}" /> to decorate.
+    /// </param>
+    /// <param name="validator"> The <see cref="IValidator{T}" /> to use for validating the request. </param>
+    public ValidateRequestHandlerDecorator(IRequestHandler<TRequest> requestHandlerToDecorate, IValidator<TRequest> validator)
+    {
+      this.requestHandlerToDecorate = requestHandlerToDecorate;
+      this.validator = validator;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Methods
+
+    /// <inheritdoc cref="IRequestHandler{TRequest, TResponse}.Handle(TRequest, CancellationToken)" />
+    public async Task Handle(TRequest request, CancellationToken cancellationToken)
+    {
+      validator.ValidateRequest(request);
+      await requestHandlerToDecorate.Handle(request, cancellationToken);
+    }
+
+    #endregion Public Methods
+  }
+
+  /// <summary>
+  /// A decorator which provides a validation as part of a request pipeline. This should be the
+  /// first in the pipeline, or at the very least before the actual request handler.
+  /// </summary>
+  /// <typeparam name="TRequest"> The request. </typeparam>
+  /// <typeparam name="TResponse"> The request response. </typeparam>
+  public sealed class ValidateRequestHandlerDecorator<TRequest, TResponse> : IRequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse>
+  {
+    #region Private Fields
+
+    private readonly IRequestHandler<TRequest, TResponse> requestHandlerToDecorate;
+    private readonly IValidator<TRequest> validator;
+
+    #endregion Private Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="ValidateRequestHandlerDecorator{TRequest}" /> class.
+    /// </summary>
+    /// <param name="requestHandlerToDecorate">
+    /// The <see cref="IRequestHandler{TRequest, TResponse}" /> to decorate.
+    /// </param>
+    /// <param name="validator"> The <see cref="IValidator{T}" /> to use for validating the request. </param>
+    public ValidateRequestHandlerDecorator(IRequestHandler<TRequest, TResponse> requestHandlerToDecorate, IValidator<TRequest> validator)
+    {
+      this.requestHandlerToDecorate = requestHandlerToDecorate;
+      this.validator = validator;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Methods
+
+    /// <inheritdoc cref="IRequestHandler{TRequest, TResponse}.Handle(TRequest, CancellationToken)" />
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+    {
+      validator.ValidateRequest(request);
+      TResponse response = await requestHandlerToDecorate.Handle(request, cancellationToken);
+      return response;
+    }
+
+    #endregion Public Methods
+  }
+}
---
+++ backend/src/PruneUrl.Backend.Application.Requests/Exceptions/InvalidRequestException.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation.Results;
+using System.Text;
+
+namespace PruneUrl.Backend.Application.Requests.Exceptions
+{
+  /// <summary>
+  /// An exception which is thrown when a <see cref="IRequest" /> or <see cref="InvalidRequest{T}"
+  /// /> is invalid.
+  /// </summary>
+  public sealed class InvalidRequestException : Exception
+  {
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="InvalidRequestException" />
+    /// </summary>
+    /// <param name="requestName"> The name of the request that is invalid. </param>
+    /// <param name="validationFailures">
+    /// The collection of <see cref="ValidationFailure" />'s to create the exception message from.
+    /// </param>
+    public InvalidRequestException(string requestName, IEnumerable<ValidationFailure> validationFailures) : base(GetExceptionMessage(requestName, validationFailures)) { }
+
+    #endregion Public Constructors
+
+    #region Private Methods
+
+    private static string GetExceptionMessage(string requestName, IEnumerable<ValidationFailure> validationFailures)
+    {
+      var builder = new StringBuilder();
+      builder.Append("The '")
+             .Append(requestName)
+             .AppendLine("' request is invalid! Validation errors:");
+      foreach (var validationFailure in validationFailures)
+      {
+        builder.Append("\t")
+               .Append("Property '")
+               .Append(validationFailure.PropertyName)
+               .Append("' is invalid: ")
+               .AppendLine(validationFailure.ErrorMessage);
+      }
+
+      return builder.ToString();
+    }
+
+    #endregion Private Methods
+  }
+}
