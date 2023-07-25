aa04549e76c9e8e9c6050ec37a7b19da1cff3059
Callum Gill <callum.j.gill@googlemail.com>
2023-07-25 17:48:37 +0100

    feat(backend): add missing GetShortUrlQueryValidator class

---
+++ backend/src/PruneUrl.Backend.Application.Queries/GetShortUrl/GetShortUrlQueryValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace PruneUrl.Backend.Application.Queries.GetShortUrl
+{
+  /// <summary>
+  /// The validator for the <see cref="GetShortUrlQuery" />.
+  /// </summary>
+  public sealed class GetShortUrlQueryValidator : AbstractValidator<GetShortUrlQuery>
+  {
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="GetShortUrlQueryValidator" /> class.
+    /// </summary>
+    public GetShortUrlQueryValidator()
+    {
+      RuleFor(query => query.ShortUrl).NotEmpty();
+    }
+
+    #endregion Public Constructors
+  }
+}
