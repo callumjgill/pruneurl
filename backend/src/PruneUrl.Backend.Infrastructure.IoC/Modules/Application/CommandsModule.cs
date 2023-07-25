1ab1373a3bc9bd748824f00b404f95f1fe84af7c
Callum Gill <callum.j.gill@googlemail.com>
2023-07-25 16:31:43 +0100

    feat(backend): add CommandsModule for autofac registrations

---
+++ backend/src/PruneUrl.Backend.Infrastructure.IoC/Modules/Application/CommandsModule.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using FluentValidation;
+using MediatR.Extensions.Autofac.DependencyInjection;
+using MediatR.Extensions.Autofac.DependencyInjection.Builder;
+using PruneUrl.Backend.Application.Commands.CreateShortUrl;
+
+namespace PruneUrl.Backend.Infrastructure.IoC.Modules.Application
+{
+  /// <summary>
+  /// The Application "Commands" specific code IoC module
+  /// </summary>
+  internal sealed class CommandsModule : Module
+  {
+    #region Protected Methods
+
+    protected override void Load(ContainerBuilder builder)
+    {
+      RegisterCreateShortUrl(builder);
+    }
+
+    #endregion Protected Methods
+
+    #region Private Methods
+
+    private void RegisterCreateShortUrl(ContainerBuilder builder)
+    {
+      MediatRConfiguration configuration = MediatRConfigurationBuilder.Create(typeof(CreateShortUrlCommand).Assembly)
+                                                                      .WithAllOpenGenericHandlerTypesRegistered()
+                                                                      .Build();
+      builder.RegisterMediatR(configuration);
+      builder.RegisterType<CreateShortUrlCommandValidator>().As<IValidator<CreateShortUrlCommand>>();
+    }
+
+    #endregion Private Methods
+  }
+}
