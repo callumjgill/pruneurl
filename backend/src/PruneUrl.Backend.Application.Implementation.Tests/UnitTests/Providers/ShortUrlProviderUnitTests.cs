19515e2772445c6d27df39bc713c81d532853262
Callum Gill <callum.j.gill@googlemail.com>
2023-07-25 11:53:06 +0100

    feat(backend): add ISequenceIdProvider & implementation for converting a short url back to its sequence id

--- backend/src/PruneUrl.Backend.Application.Implementation.Tests/UnitTests/Providers/ShortUrlProviderUnitTests.cs
+++ backend/src/PruneUrl.Backend.Application.Implementation.Tests/UnitTests/Providers/ShortUrlProviderUnitTests.cs
@@ -10,6 +10,35 @@ namespace PruneUrl.Backend.Application.Implementation.Tests.UnitTests.Providers
     #region Public Methods
 
     [Test]
+    public void GetSequenceIdTest_GeneratesExpectedSequenceId_First62CharMapsToFirst62Numbers()
+    {
+      var provider = new ShortUrlProvider();
+      char[] characterMap = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+      Assert.Multiple(() =>
+      {
+        for (int index = 0; index < characterMap.Length; index++)
+        {
+          int actualSequenceId = provider.GetSequenceId(characterMap[index].ToString());
+          Assert.That(actualSequenceId, Is.EqualTo(index));
+        }
+      });
+    }
+
+    [TestCase(int.MaxValue, "2lkCB1")]
+    [TestCase(721059154, "MNuqm")]
+    [TestCase(357302113, "obcBj")]
+    [TestCase(62, "10")]
+    [TestCase(63, "11")]
+    [TestCase(3844, "100")]
+    [TestCase(3845, "101")]
+    public void GetSequenceIdTest_GeneratesExpectedSequenceId_OtherStrings(int expectedSequenceId, string shortUrl)
+    {
+      var provider = new ShortUrlProvider();
+      int actualSequenceId = provider.GetSequenceId(shortUrl);
+      Assert.That(actualSequenceId, Is.EqualTo(expectedSequenceId));
+    }
+
+    [Test]
     public void GetShortUrlTest_GeneratesExpectedShortUrl_First62NumbersMapsToSingleChar()
     {
       var provider = new ShortUrlProvider();
