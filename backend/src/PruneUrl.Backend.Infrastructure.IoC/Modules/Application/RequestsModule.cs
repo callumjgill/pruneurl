685be6b84b8abfca813b281254daa37e1d11187e
Callum Gill <callum.j.gill@googlemail.com>
2023-07-25 18:22:57 +0100

    feat(backend): add RequestsModule for autofac registrations

---
+++ backend/src/PruneUrl.Backend.Infrastructure.IoC/Modules/Application/RequestsModule.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using Autofac.Features.Decorators;
+using FluentValidation;
+using MediatR;
+using PruneUrl.Backend.Application.Requests.Decorators;
+
+namespace PruneUrl.Backend.Infrastructure.IoC.Modules.Application
+{
+  /// <summary>
+  /// The Application "Requests" specific code IoC module.
+  /// </summary>
+  internal sealed class RequestsModule : Module
+  {
+    #region Protected Methods
+
+    protected override void Load(ContainerBuilder builder)
+    {
+      RegisterDecorators(builder);
+    }
+
+    #endregion Protected Methods
+
+    #region Private Methods
+
+    private void RegisterDecorators(ContainerBuilder builder)
+    {
+      builder.RegisterGenericDecorator(
+        typeof(ValidateRequestHandlerDecorator<>),
+        typeof(IRequestHandler<>),
+        context => ValidatorIsRegistered(typeof(IRequestHandler<>).Name, context));
+      builder.RegisterGenericDecorator(
+        typeof(ValidateRequestHandlerDecorator<,>),
+        typeof(IRequestHandler<,>),
+        context => ValidatorIsRegistered(typeof(IRequestHandler<,>).Name, context));
+    }
+
+    private bool ValidatorIsRegistered(string interfaceName, IDecoratorContext context)
+    {
+      Type? requestHandlerInterfaceType = context.ImplementationType.GetInterfaces().FirstOrDefault(interfaceType => interfaceType.Name == interfaceName);
+      Type? genericTypeArgument = requestHandlerInterfaceType?.GenericTypeArguments.FirstOrDefault(genericTypeArgument => genericTypeArgument.IsAssignableTo(typeof(IBaseRequest)));
+      return genericTypeArgument != null && context.IsRegistered(typeof(IValidator<>).MakeGenericType(genericTypeArgument));
+    }
+
+    #endregion Private Methods
+  }
+}
