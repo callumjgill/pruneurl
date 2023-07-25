9831fda22c2f72f1be0b215eaeaae58dec5d8ad9
Callum Gill <callum.j.gill@googlemail.com>
2023-07-25 15:41:51 +0100

    feat(backend): add InvalidConfigurationException and extensions for IConfiguration to the Firebase infrastructure project

---
+++ backend/src/PruneUrl.Backend.Application.Exceptions/Configuration/InvalidConfigurationException.cs
@@ -0,0 +1,20 @@
+﻿namespace PruneUrl.Backend.Application.Configuration.Exceptions
+{
+  /// <summary>
+  /// An exception which is thrown when the configuration is invalid.
+  /// </summary>
+  public sealed class InvalidConfigurationException : Exception
+  {
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="InvalidConfigurationException" /> class.
+    /// </summary>
+    /// <param name="sectionName"> The name of the configuration section which is invalid. </param>
+    public InvalidConfigurationException(string sectionName, string error) : base($"The section '{sectionName}' is invalid! {error}")
+    {
+    }
+
+    #endregion Public Constructors
+  }
+}
