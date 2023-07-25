1f29dd8a526aaa6cab4f5aa579aace9b0111c749
Callum Gill <callum.j.gill@googlemail.com>
2023-07-25 12:55:04 +0100

    feat(backend): add GetShortUrlQuery and related classes

---
+++ backend/src/PruneUrl.Backend.Application.Queries/GetShortUrl/GetShortUrlQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace PruneUrl.Backend.Application.Queries.GetShortUrl
+{
+  /// <summary>
+  /// An immutable query for getting a <see cref="Domain.Entities.ShortUrl" /> entity
+  /// </summary>
+  /// <param name="ShortUrl"> The actual "short url" value to get the corresponding entity for. </param>
+  public sealed record GetShortUrlQuery(string ShortUrl) : IRequest<GetShortUrlQueryResponse>
+  {
+  }
+}
---
+++ backend/src/PruneUrl.Backend.Application.Queries/GetShortUrl/GetShortUrlQueryResponse.cs
@@ -0,0 +1,12 @@
+﻿using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Queries.GetShortUrl
+{
+  /// <summary>
+  /// The immutable response to a <see cref="GetShortUrlQuery" />.
+  /// </summary>
+  /// <param name="ShortUrl"> The <see cref="Domain.Entities.ShortUrl" /> entity. </param>
+  public sealed record GetShortUrlQueryResponse(ShortUrl ShortUrl)
+  {
+  }
+}
