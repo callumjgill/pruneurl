8103955e48ede6a9adfb0e378bf798ab43c68c02
Callum Gill <callum.j.gill@googlemail.com>
2023-07-25 14:37:18 +0100

    feat(backend): add FirestoreDbOptions to Firestore infrastructure project

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Firestore/Configuration/FirestoreDbOptions.cs
@@ -0,0 +1,27 @@
+﻿using Google.Api.Gax;
+using Google.Cloud.Firestore;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Firestore.Configuration
+{
+  /// <summary>
+  /// A configuration DTO intended to be used in the options pattern. This is injected into classes
+  /// via the options pattern. The configuration is used for setting up a <see cref="FirestoreDb" />.
+  /// </summary>
+  public sealed class FirestoreDbOptions
+  {
+    #region Public Properties
+
+    /// <summary>
+    /// Specifies how the builder responds to the presence of the FIRESTORE_EMULATOR_HOST emulator
+    /// environment variable.
+    /// </summary>
+    public EmulatorDetection EmulatorDetection { get; set; }
+
+    /// <summary>
+    /// The ID of the Google Cloud Platform project that contains the database.
+    /// </summary>
+    public string ProjectId { get; set; } = string.Empty;
+
+    #endregion Public Properties
+  }
+}
