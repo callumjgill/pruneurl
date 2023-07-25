e10bbac30fc475740d82496102a23dfb22d9ca30
Callum Gill <callum.j.gill@googlemail.com>
2023-07-25 13:20:30 +0100

    test(backend): add unit tests for the GetShortUrlQuery classes

---
+++ backend/src/PruneUrl.Backend.Application.Queries.Tests/UnitTests/GetShortUrl/GetShortUrlQueryResponseUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Application.Queries.GetShortUrl;
+using PruneUrl.Backend.Domain.Entities;
+using PruneUrl.Backend.TestHelpers;
+
+namespace PruneUrl.Backend.Application.Queries.Tests.UnitTests.GetShortUrl
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class GetShortUrlQueryResponseUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void ConstructorTest()
+    {
+      ShortUrl shortUrl = EntityTestHelper.CreateShortUrl();
+      var response = new GetShortUrlQueryResponse(shortUrl);
+      Assert.That(response.ShortUrl, Is.EqualTo(shortUrl));
+    }
+
+    #endregion Public Methods
+  }
+}
---
+++ backend/src/PruneUrl.Backend.Application.Queries.Tests/UnitTests/GetShortUrl/GetShortUrlQueryUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Application.Queries.GetShortUrl;
+
+namespace PruneUrl.Backend.Application.Queries.Tests.UnitTests.GetShortUrl
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class GetShortUrlQueryUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void ConstructorTest()
+    {
+      const string testShortUrl = "Testing123";
+      var query = new GetShortUrlQuery(testShortUrl);
+      Assert.That(query.ShortUrl, Is.EqualTo(testShortUrl));
+    }
+
+    #endregion Public Methods
+  }
+}
