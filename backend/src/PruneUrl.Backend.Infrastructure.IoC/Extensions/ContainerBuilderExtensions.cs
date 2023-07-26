051907d7338e62f1874da688be7d41249d50e180
Callum Gill <callum.j.gill@googlemail.com>
2023-07-26 13:48:27 +0100

    feat(backend): add RegisterAllModules extension method for the Autofac ContainerBuilder

---
+++ backend/src/PruneUrl.Backend.Infrastructure.IoC/Extensions/ContainerBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using PruneUrl.Backend.Infrastructure.IoC.Modules.Application;
+using PruneUrl.Backend.Infrastructure.IoC.Modules.Infrastructure;
+
+namespace PruneUrl.Backend.Infrastructure.IoC.Extensions
+{
+  /// <summary>
+  /// Extensions for the <see cref="ContainerBuilder" /> class.
+  /// </summary>
+  public static class ContainerBuilderExtensions
+  {
+    #region Public Methods
+
+    /// <summary>
+    /// Register all the modules required for the container.
+    /// </summary>
+    public static void RegisterAllModules(this ContainerBuilder builder)
+    {
+      builder.RegisterModule<CommandsModule>();
+      builder.RegisterModule<ImplementationModule>();
+      builder.RegisterModule<QueriesModule>();
+      builder.RegisterModule<RequestsModule>();
+      builder.RegisterModule<TransactionsModule>();
+      builder.RegisterModule<FirebaseModule>();
+    }
+
+    #endregion Public Methods
+  }
+}
