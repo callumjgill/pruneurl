e621db19b9c89e7ca7743e3948b58a552069e8b2
Callum Gill <callum.j.gill@googlemail.com>
2023-07-26 11:22:16 +0100

    test(backend): add unit tests for GuidEntityIdProvider

---
+++ backend/src/PruneUrl.Backend.Application.Implementation.Tests/UnitTests/Providers/GuidEntityIdProviderUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Application.Implementation.Providers;
+
+namespace PruneUrl.Backend.Application.Implementation.Tests.UnitTests.Providers
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class GuidEntityIdProviderUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void NewIdTest()
+    {
+      var entityIdProvider = new GuidEntityIdProvider();
+      string actualId1 = entityIdProvider.NewId();
+      string actualId2 = entityIdProvider.NewId();
+      Assert.That(Guid.TryParse(actualId1, out Guid _), Is.True);
+      Assert.That(Guid.TryParse(actualId2, out Guid _), Is.True);
+      Assert.That(actualId1, Is.Not.EqualTo(actualId2));
+    }
+
+    #endregion Public Methods
+  }
+}
