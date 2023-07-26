af01ea042ea3584dfb62d3773f2199aae7d58d1d
Callum Gill <callum.j.gill@googlemail.com>
2023-07-26 16:30:37 +0100

    feat(backend): add REST methods for redirecting a short url and creating a new one

---
+++ backend/src/PruneUrl.Backend.App/Endpoints/EndpointRestMethodsUtilities.cs
@@ -0,0 +1,33 @@
+﻿namespace PruneUrl.Backend.App.Endpoints
+{
+  /// <summary>
+  /// Static class containing useful utilities to be used by the REST endpoint methods.
+  /// </summary>
+  internal static class EndpointRestMethodsUtilities
+  {
+    #region Public Methods
+
+    /// <summary>
+    /// Handles any exceptions thrown by the REST method which itself doesn't handle.
+    /// </summary>
+    /// <param name="restMethod"> The REST method the exceptions are handled from. </param>
+    /// <returns>
+    /// A task representing the asynchronous operation of executing the REST method along with any
+    /// exception handling needed.
+    /// </returns>
+    public static async Task<IResult> HandleErrors(Func<Task<IResult>> restMethod)
+    {
+      try
+      {
+        IResult result = await restMethod();
+        return result;
+      }
+      catch (Exception)
+      {
+        return Results.StatusCode(500);
+      }
+    }
+
+    #endregion Public Methods
+  }
+}
---
+++ backend/src/PruneUrl.Backend.App/Endpoints/ShortUrlEndpointRestMethods.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PruneUrl.Backend.App.Endpoints.Models;
+using PruneUrl.Backend.Application.Commands.CreateShortUrl;
+using PruneUrl.Backend.Application.Interfaces.Providers;
+using PruneUrl.Backend.Application.Transactions.GetAndBumpSequenceId;
+using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.App.Endpoints
+{
+  /// <summary>
+  /// Static class containing the methods for the <see cref="ShortUrl" /> entity resource REST endpoint.
+  /// </summary>
+  internal static class ShortUrlEndpointRestMethods
+  {
+    #region Public Methods
+
+    /// <summary>
+    /// The POST REST Endpoint for creating a new <see cref="ShortUrl" /> entity.
+    /// </summary>
+    /// <param name="requestBody">
+    /// The request body containing data that will be used to create a <see cref="ShortUrl" />
+    /// entity from.
+    /// </param>
+    /// <param name="mediator">
+    /// The <see cref="IMediator" /> interface used to send requests to the underlying database.
+    /// </param>
+    /// <returns>
+    /// A task representing the asynchronous operation of creating a new <see cref="ShortUrl" /> entity.
+    /// </returns>
+    public static Task<IResult> PostShortUrl([FromBody] ShortUrlPostRequest requestBody,
+                                             [FromServices] IMediator mediator,
+                                             [FromServices] IShortUrlProvider shortUrlProvider)
+    {
+      return EndpointRestMethodsUtilities.HandleErrors(async () =>
+      {
+        var sequenceIdRequest = new GetAndBumpSequenceIdRequest();
+        GetAndBumpSequenceIdResponse sequenceIdResponse = await mediator.Send(sequenceIdRequest);
+        int sequenceId = sequenceIdResponse.SequenceId.Value;
+        var command = new CreateShortUrlCommand(requestBody.LongUrl, sequenceId);
+        await mediator.Send(command);
+        string shortUrl = shortUrlProvider.GetShortUrl(sequenceId);
+        return Results.CreatedAtRoute(RouteNames.RedirectRoute, shortUrl);
+      });
+    }
+
+    #endregion Public Methods
+  }
+}
