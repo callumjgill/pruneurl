3a9d5c4205d1b78ac1359901c9590bfc6e497565
Callum Gill <callum.j.gill@googlemail.com>
2023-07-26 13:48:13 +0100

    feat(backend): add TransactionModule for autofac registrations

---
+++ backend/src/PruneUrl.Backend.Infrastructure.IoC/Modules/Application/TransactionsModule.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using MediatR.Extensions.Autofac.DependencyInjection;
+using MediatR.Extensions.Autofac.DependencyInjection.Builder;
+using PruneUrl.Backend.Application.Transactions.GetAndBumpSequenceId;
+
+namespace PruneUrl.Backend.Infrastructure.IoC.Modules.Application
+{
+  /// <summary>
+  /// The Application "Transactions" specific code IoC module.
+  /// </summary>
+  internal sealed class TransactionsModule : Module
+  {
+    #region Protected Methods
+
+    protected override void Load(ContainerBuilder builder)
+    {
+      RegisterGetAndBumpSequenceId(builder);
+    }
+
+    #endregion Protected Methods
+
+    #region Private Methods
+
+    private void RegisterGetAndBumpSequenceId(ContainerBuilder builder)
+    {
+      MediatRConfiguration configuration = MediatRConfigurationBuilder.Create(typeof(GetAndBumpSequenceIdRequest).Assembly)
+                                                                      .WithAllOpenGenericHandlerTypesRegistered()
+                                                                      .Build();
+      builder.RegisterMediatR(configuration);
+    }
+
+    #endregion Private Methods
+  }
+}
