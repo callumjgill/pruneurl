0e52d35231251cc52597fd692fbe0b1980e9d482
Callum Gill <callum.j.gill@googlemail.com>
2023-07-26 14:56:11 +0100

    test(backend): add unit tests for FirestoreDbOptions

---
+++ backend/src/PruneUrl.Backend.Infrastructure.Database.Firestore.Tests/UnitTests/Configuration/FirestoreDbOptionsUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using Google.Api.Gax;
+using NUnit.Framework;
+using PruneUrl.Backend.Infrastructure.Database.Firestore.Configuration;
+
+namespace PruneUrl.Backend.Infrastructure.Database.Firestore.Tests.UnitTests.Configuration
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class FirestoreDbOptionsUnitTests
+  {
+    #region Public Methods
+
+    [TestCase(EmulatorDetection.None)]
+    [TestCase(EmulatorDetection.ProductionOnly)]
+    [TestCase(EmulatorDetection.EmulatorOrProduction)]
+    [TestCase(EmulatorDetection.EmulatorOnly)]
+    public void EmulatorDetectionTest(EmulatorDetection emulatorDetection)
+    {
+      var firestoreDbOptions = new FirestoreDbOptions();
+      Assert.That(firestoreDbOptions.EmulatorDetection, Is.EqualTo(default(EmulatorDetection)));
+      firestoreDbOptions.EmulatorDetection = emulatorDetection;
+      Assert.That(firestoreDbOptions.EmulatorDetection, Is.EqualTo(emulatorDetection));
+    }
+
+    [Test]
+    public void ProjectIdTest()
+    {
+      string testProjectId = "Testing123";
+      var firestoreDbOptions = new FirestoreDbOptions();
+      Assert.That(firestoreDbOptions.ProjectId, Is.EqualTo(string.Empty));
+      firestoreDbOptions.ProjectId = testProjectId;
+      Assert.That(firestoreDbOptions.ProjectId, Is.EqualTo(testProjectId));
+    }
+
+    #endregion Public Methods
+  }
+}
