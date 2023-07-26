53743bb1609ad9b2f2cd3e485a9ab2aa94fd6a80
Callum Gill <callum.j.gill@googlemail.com>
2023-07-26 17:42:41 +0100

    feat(backend): add REST minimal API route mapping

---
+++ backend/src/PruneUrl.Backend.App/Endpoints/EndpointRoutes.cs
@@ -0,0 +1,67 @@
+using Microsoft.OpenApi.Models;
+using PruneUrl.Backend.App.Endpoints.Models;
+
+namespace PruneUrl.Backend.App.Endpoints
+{
+  /// <summary>
+  /// Provides API REST routes for the app.
+  /// </summary>
+  internal static class EndpointRoutes
+  {
+    #region Private Fields
+
+    private const string ShortUrlResourceGroup = "/shortUrls";
+
+    #endregion Private Fields
+
+    #region Public Methods
+
+    /// <summary>
+    /// Maps all the REST endpoint routes.
+    /// </summary>
+    /// <param name="routeBuilder"> </param>
+    /// <returns> </returns>
+    public static IEndpointRouteBuilder MapEndpointRoutes(this IEndpointRouteBuilder routeBuilder)
+    {
+      routeBuilder.MapRedirectRoutes()
+                  .MapShortUrlRoutes();
+
+      return routeBuilder;
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    private static IEndpointRouteBuilder MapRedirectRoutes(this IEndpointRouteBuilder routeBuilder)
+    {
+      routeBuilder.MapGet("/{shortUrl}", RedirectEndpointRestMethods.GetShortUrl)
+                  .WithName(RouteNames.PostShortUrlRoute)
+                  .Produces(StatusCodes.Status307TemporaryRedirect)
+                  .Produces(StatusCodes.Status500InternalServerError);
+
+      return routeBuilder;
+    }
+
+    private static IEndpointRouteBuilder MapShortUrlRoutes(this IEndpointRouteBuilder routeBuilder)
+    {
+      RouteGroupBuilder shortUrlResourceGroup = routeBuilder.MapGroup(ShortUrlResourceGroup);
+
+      shortUrlResourceGroup.MapPost("/", ShortUrlEndpointRestMethods.PostShortUrl)
+                           .WithName(RouteNames.PostShortUrlRoute)
+                           .WithOpenApi(generatedOperation =>
+                           {
+                             generatedOperation.Summary = "Creates a new 'short url' resource.";
+                             generatedOperation.Description = "Creates a new 'short url' resource and returns its location if successfully created.";
+                             return generatedOperation;
+                           })
+                           .Accepts<ShortUrlPostRequest>("application/json")
+                           .Produces(StatusCodes.Status201Created)
+                           .Produces(StatusCodes.Status500InternalServerError);
+
+      return routeBuilder;
+    }
+
+    #endregion Private Methods
+  }
+}
