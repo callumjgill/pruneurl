395c5c6e6ed4aaab633fc0af6c81a88a57f98077
Callum Gill <callum.j.gill@googlemail.com>
2023-07-26 17:03:09 +0100

    feat(backend): add console logger, autofac registration, http logging, https redirection and exception handler to the App program

--- backend/src/PruneUrl.Backend.App/Program.cs
+++ backend/src/PruneUrl.Backend.App/Program.cs
@@ -1,6 +1,26 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using PruneUrl.Backend.Infrastructure.IoC.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders() // Remove defaults
+               .AddConsole();
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterAllModules());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
+app.UseHttpLogging();
+
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler(exceptionHandlerApp => exceptionHandlerApp.Run(async context => await Results.Problem().ExecuteAsync(context)));
+}
+
+app.UseHttpsRedirection();
+
+await app.RunAsync();
