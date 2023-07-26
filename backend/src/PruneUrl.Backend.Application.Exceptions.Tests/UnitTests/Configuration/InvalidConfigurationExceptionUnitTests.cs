e6fb052a0932bb238e7b406d44b70235f4cb2afd
Callum Gill <callum.j.gill@googlemail.com>
2023-07-26 14:54:41 +0100

    test(backend): add unit tests for InvalidConfigurationException

---
+++ backend/src/PruneUrl.Backend.Application.Exceptions.Tests/UnitTests/Configuration/InvalidConfigurationExceptionUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Application.Configuration.Exceptions;
+
+namespace PruneUrl.Backend.Application.Exceptions.Tests.UnitTests.Configuration
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class InvalidConfigurationExceptionUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void ConstructorTest()
+    {
+      const string sectionName = "TestSection";
+      const string error = "TestError";
+      var invalidConfigException = new InvalidConfigurationException(sectionName, error);
+      Assert.That(invalidConfigException.Message, Is.EqualTo($"The section '{sectionName}' is invalid! {error}"));
+    }
+
+    #endregion Public Methods
+  }
+}
