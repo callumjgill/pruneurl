d1bb7eefd1aa37f6483ca7b0f2a3bfa4c675f912
Callum Gill <callum.j.gill@googlemail.com>
2023-07-27 14:43:05 +0100

    feat(backend): add 'GetSequenceId' query

---
+++ backend/src/PruneUrl.Backend.Application.Queries/GetSequenceId/GetSequenceIdQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using PruneUrl.Backend.Application.Interfaces.Database.Operations.Read;
+using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Queries.GetSequenceId
+{
+  /// <summary>
+  /// The handler for the <see cref="GetSequenceIdQuery" /> request.
+  /// </summary>
+  public sealed class GetSequenceIdQueryHandler : IRequestHandler<GetSequenceIdQuery, GetSequenceIdQueryResponse>
+  {
+    #region Private Fields
+
+    private readonly IDbGetByIdOperation<SequenceId> dbGetByIdOperation;
+
+    #endregion Private Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="GetSequenceIdQueryHandler" /> class.
+    /// </summary>
+    /// <param name="dbGetByIdOperation">
+    /// The operation for retrieving a <see cref="SequenceId" /> by its id.
+    /// </param>
+    public GetSequenceIdQueryHandler(IDbGetByIdOperation<SequenceId> dbGetByIdOperation)
+    {
+      this.dbGetByIdOperation = dbGetByIdOperation;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Methods
+
+    /// <inheritdoc cref="IRequestHandler{TRequest, TResponse}.Handle(TRequest, CancellationToken)" />
+    public async Task<GetSequenceIdQueryResponse> Handle(GetSequenceIdQuery request, CancellationToken cancellationToken)
+    {
+      SequenceId? sequenceId = await dbGetByIdOperation.GetByIdAsync(request.Id, cancellationToken);
+      return new GetSequenceIdQueryResponse(sequenceId);
+    }
+
+    #endregion Public Methods
+  }
+}
