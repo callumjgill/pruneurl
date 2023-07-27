92af2338655fc246f2f39b90b374a493f3494f5f
Callum Gill <callum.j.gill@googlemail.com>
2023-07-27 14:41:22 +0100

    feat(backend): add 'CreateSequenceId' command

---
+++ backend/src/PruneUrl.Backend.Application.Commands/CreateSequenceId/CreateSequenceIdCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Commands.CreateSequenceId
+{
+  /// <summary>
+  /// The immutable command for creating a new <see cref="SequenceId" /> entity.
+  /// </summary>
+  /// <param name="Id"> The unique identifier for the <see cref="SequenceId" /> entity. </param>
+  public sealed record CreateSequenceIdCommand(string Id) : IRequest
+  {
+  }
+}
