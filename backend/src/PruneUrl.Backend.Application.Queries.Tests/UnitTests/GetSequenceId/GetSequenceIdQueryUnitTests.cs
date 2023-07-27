68307fb8c423eae3cd9f6784baa25bfe49bdb92d
Callum Gill <callum.j.gill@googlemail.com>
2023-07-27 14:43:27 +0100

    test(backend): add unit tests for 'GetSequenceId' query

---
+++ backend/src/PruneUrl.Backend.Application.Queries.Tests/UnitTests/GetSequenceId/GetSequenceIdQueryUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Application.Queries.GetSequenceId;
+
+namespace PruneUrl.Backend.Application.Queries.Tests.UnitTests.GetSequenceId
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class GetSequenceIdQueryUnitTests
+  {
+    #region Public Methods
+
+    [Test]
+    public void ConstructorTest()
+    {
+      const string testId = "Testing123";
+      var query = new GetSequenceIdQuery(testId);
+      Assert.That(query.Id, Is.EqualTo(testId));
+    }
+
+    #endregion Public Methods
+  }
+}
