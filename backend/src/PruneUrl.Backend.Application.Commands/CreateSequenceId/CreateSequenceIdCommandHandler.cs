c5838df5aea5a574f1a05f4e10c32e733bf11699
Callum Gill <callum.j.gill@googlemail.com>
2023-07-27 14:41:22 +0100

    feat(backend): add 'CreateSequenceId' command

---
+++ backend/src/PruneUrl.Backend.Application.Commands/CreateSequenceId/CreateSequenceIdCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using PruneUrl.Backend.Application.Interfaces.Database.Requests;
+using PruneUrl.Backend.Application.Interfaces.Factories.Entities;
+using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Commands.CreateSequenceId
+{
+  /// <summary>
+  /// The handler for the <see cref="CreateSequenceIdCommand" /> request.
+  /// </summary>
+  public sealed class CreateSequenceIdCommandHandler : IRequestHandler<CreateSequenceIdCommand>
+  {
+    #region Private Fields
+
+    private readonly IDbWriteBatch<SequenceId> dbWriteBatch;
+    private readonly ISequenceIdFactory sequenceIdFactory;
+
+    #endregion Private Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="CreateSequenceIdCommandHandler" /> class.
+    /// </summary>
+    /// <param name="dbWriteBatch">
+    /// The batch of operations for the <see cref="SequenceId" /> entity which will be committed to
+    /// the database.
+    /// </param>
+    /// <param name="sequenceIdFactory">
+    /// The factory for creating instances of the <see cref="SequenceId" /> type.
+    /// </param>
+    public CreateSequenceIdCommandHandler(IDbWriteBatch<SequenceId> dbWriteBatch,
+                                          ISequenceIdFactory sequenceIdFactory)
+    {
+      this.dbWriteBatch = dbWriteBatch;
+      this.sequenceIdFactory = sequenceIdFactory;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Methods
+
+    /// <inheritdoc cref="IRequestHandler{TRequest}.Handle(TRequest, CancellationToken)" />
+    public Task Handle(CreateSequenceIdCommand request, CancellationToken cancellationToken)
+    {
+      SequenceId newSequenceId = sequenceIdFactory.Create(request.Id, 0);
+      dbWriteBatch.Create(newSequenceId);
+      return dbWriteBatch.CommitAsync(cancellationToken);
+    }
+
+    #endregion Public Methods
+  }
+}
---
+++ backend/src/PruneUrl.Backend.Application.Commands/CreateSequenceId/CreateSequenceIdCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace PruneUrl.Backend.Application.Commands.CreateSequenceId
+{
+  /// <summary>
+  /// A validator for the <see cref="CreateSequenceIdCommand" />.
+  /// </summary>
+  public sealed class CreateSequenceIdCommandValidator : AbstractValidator<CreateSequenceIdCommand>
+  {
+    #region Public Constructors
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="CreateSequenceIdCommandValidator" /> class.
+    /// </summary>
+    public CreateSequenceIdCommandValidator()
+    {
+      RuleFor(command => command.Id).NotEmpty();
+    }
+
+    #endregion Public Constructors
+  }
+}
