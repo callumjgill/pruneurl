f0aca38957c3b40d0ce90914ecb4b4691039589d
Callum Gill <callum.j.gill@googlemail.com>
2023-07-27 14:43:27 +0100

    test(backend): add unit tests for 'GetSequenceId' query

---
+++ backend/src/PruneUrl.Backend.Application.Queries.Tests/UnitTests/GetSequenceId/GetSequenceIdQueryHandlerUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using PruneUrl.Backend.Application.Interfaces.Database.Operations.Read;
+using PruneUrl.Backend.Application.Queries.GetSequenceId;
+using PruneUrl.Backend.Domain.Entities;
+using PruneUrl.Backend.TestHelpers;
+
+namespace PruneUrl.Backend.Application.Queries.Tests.UnitTests.GetSequenceId
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class GetSequenceIdQueryHandlerUnitTests
+  {
+    #region Public Methods
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public async Task HandleTest(bool isNull)
+    {
+      const string testId = "Testing123";
+      var dbGetByIdOperation = Substitute.For<IDbGetByIdOperation<SequenceId>>();
+      SequenceId? testSequenceId = isNull ? null : EntityTestHelper.CreateSequenceId();
+
+      dbGetByIdOperation.GetByIdAsync(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(testSequenceId);
+
+      var cancellationToken = CancellationToken.None;
+      var query = new GetSequenceIdQuery(testId);
+      var handler = new GetSequenceIdQueryHandler(dbGetByIdOperation);
+      GetSequenceIdQueryResponse response = await handler.Handle(query, cancellationToken);
+
+      Assert.That(response.SequenceId, Is.EqualTo(testSequenceId));
+      await dbGetByIdOperation.Received(1).GetByIdAsync(Arg.Any<string>(), Arg.Any<CancellationToken>());
+      await dbGetByIdOperation.Received(1).GetByIdAsync(testId, cancellationToken);
+    }
+
+    #endregion Public Methods
+  }
+}
---
+++ backend/src/PruneUrl.Backend.Application.Queries.Tests/UnitTests/GetSequenceId/GetSequenceIdQueryResponseUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using PruneUrl.Backend.Application.Queries.GetSequenceId;
+using PruneUrl.Backend.Domain.Entities;
+using PruneUrl.Backend.TestHelpers;
+
+namespace PruneUrl.Backend.Application.Queries.Tests.UnitTests.GetSequenceId
+{
+  [TestFixture]
+  [Parallelizable]
+  public sealed class GetSequenceIdQueryResponseUnitTests
+  {
+    #region Public Methods
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public void ConstructorTest(bool isNull)
+    {
+      SequenceId? testSequenceId = isNull ? null : EntityTestHelper.CreateSequenceId();
+      var response = new GetSequenceIdQueryResponse(testSequenceId);
+      Assert.That(response.SequenceId, Is.EqualTo(testSequenceId));
+    }
+
+    #endregion Public Methods
+  }
+}
