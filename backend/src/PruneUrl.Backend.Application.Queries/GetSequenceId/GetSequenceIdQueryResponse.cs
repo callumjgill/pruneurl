484e83212d5b4cd63633ae38a668a75971831cc7
Callum Gill <callum.j.gill@googlemail.com>
2023-07-27 14:43:05 +0100

    feat(backend): add 'GetSequenceId' query

---
+++ backend/src/PruneUrl.Backend.Application.Queries/GetSequenceId/GetSequenceIdQueryResponse.cs
@@ -0,0 +1,14 @@
+﻿using PruneUrl.Backend.Domain.Entities;
+
+namespace PruneUrl.Backend.Application.Queries.GetSequenceId
+{
+  /// <summary>
+  /// The immutable response to a <see cref="GetSequenceIdQuery" /> request.
+  /// </summary>
+  /// <param name="SequenceId">
+  /// The <see cref="Domain.Entities.SequenceId" /> entity found, or <c> null </c> if not found.
+  /// </param>
+  public sealed record GetSequenceIdQueryResponse(SequenceId? SequenceId)
+  {
+  }
+}
