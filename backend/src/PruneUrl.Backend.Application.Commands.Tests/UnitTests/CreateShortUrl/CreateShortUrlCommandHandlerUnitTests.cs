50a5a88a1eba3c0ab7600af6eebf23af2e41a806
Callum Gill <callum.j.gill@googlemail.com>
2023-08-10 14:37:04 +0100

    chore(deps): replace Moq with NSubstitute in PruneUrl.Backend.Application.Commands.Tests project

--- backend/src/PruneUrl.Backend.Application.Commands.Tests/UnitTests/CreateShortUrl/CreateShortUrlCommandHandlerUnitTests.cs
+++ backend/src/PruneUrl.Backend.Application.Commands.Tests/UnitTests/CreateShortUrl/CreateShortUrlCommandHandlerUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using NSubstitute;
+using NSubstitute.ReceivedExtensions;
 using NUnit.Framework;
 using PruneUrl.Backend.Application.Commands.CreateShortUrl;
 using PruneUrl.Backend.Application.Interfaces.Database.Requests;
@@ -19,25 +20,25 @@ namespace PruneUrl.Backend.Application.Commands.Tests.UnitTests.CreateShortUrl
     {
       string testLongUrl = "https://www.youtube.com";
       int testSequenceId = 6124323;
-      var dbWriteBatchMock = new Mock<IDbWriteBatch<ShortUrl>>();
-      var shortUrlFactoryMock = new Mock<IShortUrlFactory>();
+      var dbWriteBatch = Substitute.For<IDbWriteBatch<ShortUrl>>();
+      var shortUrlFactory = Substitute.For<IShortUrlFactory>();
       ShortUrl testShortUrl = EntityTestHelper.CreateShortUrl();
       CancellationToken cancellationToken = CancellationToken.None;
 
-      shortUrlFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<int>())).Returns(testShortUrl);
-      dbWriteBatchMock.Setup(x => x.CommitAsync(It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
+      shortUrlFactory.Create(Arg.Any<string>(), Arg.Any<int>()).Returns(testShortUrl);
+      dbWriteBatch.CommitAsync(Arg.Any<CancellationToken>()).Returns(Task.CompletedTask);
 
-      var handler = new CreateShortUrlCommandHandler(dbWriteBatchMock.Object, shortUrlFactoryMock.Object);
+      var handler = new CreateShortUrlCommandHandler(dbWriteBatch, shortUrlFactory);
       var command = new CreateShortUrlCommand(testLongUrl, testSequenceId);
 
       await handler.Handle(command, cancellationToken);
 
-      shortUrlFactoryMock.Verify(x => x.Create(It.IsAny<string>(), It.IsAny<int>()), Times.Once);
-      shortUrlFactoryMock.Verify(x => x.Create(testLongUrl, testSequenceId), Times.Once);
-      dbWriteBatchMock.Verify(x => x.Create(It.IsAny<ShortUrl>()), Times.Once);
-      dbWriteBatchMock.Verify(x => x.Create(testShortUrl), Times.Once);
-      dbWriteBatchMock.Verify(x => x.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
-      dbWriteBatchMock.Verify(x => x.CommitAsync(cancellationToken), Times.Once);
+      shortUrlFactory.Received(1).Create(Arg.Any<string>(), Arg.Any<int>());
+      shortUrlFactory.Received(1).Create(testLongUrl, testSequenceId);
+      dbWriteBatch.Received(1).Create(Arg.Any<ShortUrl>());
+      dbWriteBatch.Received(1).Create(testShortUrl);
+      await dbWriteBatch.Received(1).CommitAsync(Arg.Any<CancellationToken>());
+      await dbWriteBatch.Received(1).CommitAsync(cancellationToken);
     }
 
     #endregion Public Methods
