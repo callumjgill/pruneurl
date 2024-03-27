c9ba6ffb82ab4dab9709020c213527a3c1c3f997
Callum Gill <callum.j.gill@googlemail.com>
2024-03-27 10:21:52 +0000

    refactor(backend): use MediatR behaviours instead of decorators
    Closes #93

---
+++ backend/src/PruneUrl.Backend.Application.Requests.Tests/UnitTests/Behaviors/ValidateRequestBehaviorUnitTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace PruneUrl.Backend.Application.Requests.Tests;
+
+[TestFixture]
+[Parallelizable]
+public sealed class ValidateRequestBehaviorUnitTests
+{
+  [Test]
+  public void HandleTest_Response_IsNotValid()
+  {
+    IValidator<IRequest<StubResponse>> validator = Substitute.For<
+      IValidator<IRequest<StubResponse>>
+    >();
+    ValidationResult validationResult = Substitute.For<ValidationResult>();
+    IRequest<StubResponse> request = Substitute.For<IRequest<StubResponse>>();
+    StubResponse response = Substitute.For<StubResponse>();
+    CancellationToken cancellationToken = CancellationToken.None;
+    Task<StubResponse> next() => Task.FromResult(response);
+
+    validationResult.IsValid.Returns(false);
+    validator.Validate(Arg.Any<IRequest<StubResponse>>()).Returns(validationResult);
+
+    ValidateRequestBehavior<IRequest<StubResponse>, StubResponse> behaviour = new(validator);
+    Assert.That(
+      async () => await behaviour.Handle(request, next, cancellationToken),
+      Throws.TypeOf<InvalidRequestException>()
+    );
+    validator.Received(1).Validate(Arg.Any<IRequest<StubResponse>>());
+    validator.Received(1).Validate(request);
+  }
+
+  [Test]
+  public async Task HandleTest_Response_IsValid()
+  {
+    IValidator<IRequest<StubResponse>> validator = Substitute.For<
+      IValidator<IRequest<StubResponse>>
+    >();
+    ValidationResult validationResult = Substitute.For<ValidationResult>();
+    IRequest<StubResponse> request = Substitute.For<IRequest<StubResponse>>();
+    StubResponse response = Substitute.For<StubResponse>();
+    CancellationToken cancellationToken = CancellationToken.None;
+    Task<StubResponse> next() => Task.FromResult(response);
+
+    validationResult.IsValid.Returns(true);
+    validator.Validate(Arg.Any<IRequest<StubResponse>>()).Returns(validationResult);
+
+    ValidateRequestBehavior<IRequest<StubResponse>, StubResponse> decorator = new(validator);
+    StubResponse actualResponse = await decorator.Handle(request, next, cancellationToken);
+    Assert.That(actualResponse, Is.EqualTo(response));
+    validator.Received(1).Validate(Arg.Any<IRequest<StubResponse>>());
+    validator.Received(1).Validate(request);
+  }
+
+  internal abstract class StubResponse { }
+}
