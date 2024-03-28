618bc7d53569b7059ad6c0198936235bda6ae3c7
Callum Gill <callum.j.gill@googlemail.com>
2024-03-28 07:36:47 +0000

    feat(backend): add logging behavior to MediatR request pipeline

---
+++ backend/src/PruneUrl.Backend.TestHelpers/LoggerTestHelper.cs
@@ -0,0 +1,43 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace PruneUrl.Backend.TestHelpers;
+
+/// <summary>
+/// Helper class for the <see cref="ILogger"/>
+/// </summary>
+public static class LoggerTestHelper
+{
+  /// <summary>
+  /// Verifies a call to the "Log" method of a substituted <see cref="ILogger{TCategoryName}"/> is made with the expected message and log level.
+  /// There is also an optional exception that can be passed if an exception is expected to be logged too.
+  /// </summary>
+  /// <typeparam name="TCategoryName">The type whose name is used for the logger category name.</typeparam>
+  /// <param name="logger">This <see cref="ILogger"/> (substitution).</param>
+  /// <param name="expectedMessage">The expected message to be logged.</param>
+  /// <param name="expectedLogLevel">The expected log level to have been used.</param>
+  /// <param name="invocations">The number of invocations of the method to be expected.</param>
+  /// <param name="ex">The exception logged, if any.</param>
+  public static void RecievedLogWithStringMessage<TCategoryName>(
+    this ILogger<TCategoryName> logger,
+    string expectedMessage,
+    LogLevel expectedLogLevel,
+    int invocations,
+    Exception? ex = null
+  )
+  {
+    logger
+      .Received(invocations)
+      .Log(
+        expectedLogLevel,
+        Arg.Any<EventId>(),
+        Arg.Is<Arg.AnyType>(
+          (object o) =>
+            string.Equals(expectedMessage, o.ToString(), StringComparison.InvariantCulture)
+        ),
+        ex,
+        Arg.Any<Func<Arg.AnyType, Exception?, string>>()
+      );
+  }
+}
