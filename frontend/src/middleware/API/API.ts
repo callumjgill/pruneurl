ecc4684eb2a0fd5c87203f723c200874e9652625
Callum Gill <callum.j.gill@googlemail.com>
2024-05-11 10:27:28 +0100

    refactor(frontend): the API middleware now uses DTO responses

--- frontend/src/middleware/API/API.ts
+++ frontend/src/middleware/API/API.ts
@@ -1,5 +1,5 @@
-import PruneUrlResult from "./DTOs/PruneUrlResult";
+import { UrlResult } from "./DTOs";
 
 export default interface API {
-  pruneUrl: (longUrl: string) => Promise<PruneUrlResult>;
+  pruneUrl: (longUrl: string) => Promise<UrlResult>;
 }
