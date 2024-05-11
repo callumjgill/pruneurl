afd52ae2a1af773eda807478659d96ead6180bb3
Callum Gill <callum.j.gill@googlemail.com>
2024-05-11 10:43:43 +0100

    refactor(frontend): replace getApi function with hook

--- frontend/src/middleware/API/utils/shouldUseDummyAPI.ts
+++
@@ -1,8 +0,0 @@
-const shouldUseDummyAPI = (): boolean => {
-  return (
-    import.meta.env.VITE_USE_DUMMY_API !== undefined &&
-    import.meta.env.VITE_USE_DUMMY_API === "true"
-  );
-};
-
-export default shouldUseDummyAPI;
